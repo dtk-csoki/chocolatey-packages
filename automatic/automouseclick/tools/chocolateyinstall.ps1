897cd647777ddfc5e08984911dc5d30eae4ebd19
Chocolatey <chocolatey@realdimensions.net>
2022-07-13 16:25:58 +0000

    AU: 2 updated - automouseclick tesseract [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/137d260c5142a2503618ef8e71125e6ed3147481

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'D93934C83931AC5514E313C99DEFFB54757CD111E2F5CEBC2A4B37E680758D81'
+  checksum     = '2999ED4952E02ADA0FEB1A033E6C507EE06F40E5F98F495D7DE04F6EA1C62CB2'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/tesseract/tools/chocolateyinstall.ps1
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.2.0.20220708.exe"
-  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.2.0.20220708.exe"
+  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.2.0.20220712.exe"
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.2.0.20220712.exe"
   silentArgs  = '/S'
 }
 
