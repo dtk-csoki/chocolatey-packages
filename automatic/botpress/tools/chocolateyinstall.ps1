1c123cc90dc97c472305e1b6337420111cde0acc
Chocolatey <chocolatey@realdimensions.net>
2022-07-27 08:26:03 +0000

    AU: 1 updated - botpress [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d119f0c427b7014008aa3c6996a447af360ca9f7

--- automatic/botpress/tools/chocolateyinstall.ps1
+++ automatic/botpress/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $botpressDir
   
-  url           = 'https://s3.amazonaws.com/botpress-binaries/botpress-v12_28_1-win-x64.zip'  
-  checksum      = 'a195ecf68897cb5f11ad00f896ecd99b5baacac1816aedf0e46badd0d77be633'
+  url           = 'https://s3.amazonaws.com/botpress-binaries/botpress-v12_29_0-win-x64.zip'  
+  checksum      = 'b629a8237f3ead1a168b8db9ce05ad128215bc9c4d395dbbb6c66ee6f95d1419'
   checksumType  = 'sha256'
 }
 
