e7939a97144f6bc86ae17d093af14dc05c4f338b
Chocolatey <chocolatey@realdimensions.net>
2022-08-13 08:25:55 +0000

    AU: 1 updated - jtalert [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d947da53fc47af128f418a71e56695cf66380d85

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dnl.hamapps.com/JTAlert/2dc5d875bb4da19b1fcdad70bc254de4'
-  checksum     = 'e2a2a3300653d275cc5e0d4751d56b396ca5ef64a3d9399aef22b5fac0e87d8c'
+  url          = 'https://dnl.hamapps.com/JTAlert/c9b97ea0dad65d690176dd085b7d7355'
+  checksum     = '39c218e164e7c0451cc7d0c7e708adb3540191ae5a110659a312f74a518e8b28'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
