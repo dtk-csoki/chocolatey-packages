f07ccf66be5d1fbb446a391b61d6a22156b20c06
Chocolatey <chocolatey@realdimensions.net>
2022-08-16 16:26:10 +0000

    AU: 1 updated - virtualhere-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e3ba3165bb2fdc66247266247bf940ec4a670a3f

--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
-  checksum64     = '46926f78db119fec9369ad7f7176b2e4a02159a3c03f7e673e1a3b1c096b0849'
+  checksum64     = '2e515f3ac95dc6955098a68b045d76426c26e59ba668862611930127f2bef45d'
   checksumType64 = 'sha256'
 }
 
