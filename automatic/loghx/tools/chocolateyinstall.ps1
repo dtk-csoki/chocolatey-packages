577d58b594f4fab0483a9d03e2330923e68b9476
Chocolatey <chocolatey@realdimensions.net>
2022-08-18 00:25:48 +0000

    AU: 3 updated - loghx myfamilytree myfamilytree-languagepack [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/dca2a944cd10968b760d5c0d36bb39a87072ea88

--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_702.exe'
-  checksum      = 'c6b72abb0c3561b35f4c8b207adfd8c9e8cccf3b59a75df289fe4052bac794cf'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_703.exe'
+  checksum      = '388d908c7c0ff9424d8a7e929af8153ad62ab10c75775ef24ee19dfe80adf881'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/myfamilytree/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   filetype      = 'msi'
 
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.1.2.0.msi'
-  checksum      = 'fb648cf42fda8a69a3bca91d5786bc84cc62c99257eaec30ed333d1b1478c05c'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.2.0.0.msi'
+  checksum      = '76f9406bc09d488f936ee84be490979df257cb9b672c32fb3af8dda3c4e0f177'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.1.2.0x64.msi'
-  checksum64    = '7e62cbeea583a229f70978953d0c80ad3d0d16f0843e070f471038ca45204a1e'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.2.0.0x64.msi'
+  checksum64    = '26985ea189e2d7df338e91d6fc2afdb4a4c079f0ff7c33c37271455adfbd49fe'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
