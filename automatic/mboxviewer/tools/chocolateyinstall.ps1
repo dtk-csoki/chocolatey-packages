7251d283648e1fff288be12b6a69e4f84c8b443f
Chocolatey <chocolatey@realdimensions.net>
2022-08-20 08:25:50 +0000

    AU: 1 updated - mboxviewer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f86ca3f8e9a79af668a4ccefceb339ab9447cc4f

--- automatic/mboxviewer/tools/chocolateyinstall.ps1
+++ automatic/mboxviewer/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.32.zip"
+  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.33.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
