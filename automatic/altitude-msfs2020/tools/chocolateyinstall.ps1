0c5e758fe122e37fc6248536870f3e863e01c0d1
Chocolatey <chocolatey@realdimensions.net>
2022-08-20 00:26:06 +0000

    AU: 2 updated - altitude-msfs2020 loghx [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5fa8ed0db570bb63df1c8aa6f484b53d73ca1461

--- automatic/altitude-msfs2020/tools/chocolateyinstall.ps1
+++ automatic/altitude-msfs2020/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   destination   = "$toolsDir"
 
   url           = 'https://api.ivao.aero/v2/softwares/altitude/5/files/latest/download'
-  checksum      = '17f5e9ae3a03524e0c9d80524930ad1baf77ab0df2dceeaddd4f182b6743576c'
+  checksum      = '7c7f6c6f87377cc289e6f0c0a8f9d2c8bceb1d045bfb57b2e00eb2cbd39f8689'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_703.exe'
-  checksum      = '388d908c7c0ff9424d8a7e929af8153ad62ab10c75775ef24ee19dfe80adf881'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_704.exe'
+  checksum      = 'fc762626c4615517cf1ec101fb1d6cfb143d7de26af3e37d3c9a3d19a232ee61'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
