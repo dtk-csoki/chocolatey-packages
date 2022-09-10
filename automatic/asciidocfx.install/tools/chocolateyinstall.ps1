65c3b13afb04506c8e563861163ab8783fdeaf1a
chtof <chtof62@gmail.com>
2022-09-11 00:05:56 +0200

    asciidocfx.install: package change as binary > 200 MB

--- automatic/asciidocfx.install/tools/chocolateyinstall.ps1
+++ automatic/asciidocfx.install/tools/chocolateyinstall.ps1
@@ -2,9 +2,14 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\AsciidocFX_Windows.exe"
-  silentArgs  = '-q'
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+
+  url           = 'https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.8/AsciidocFX_Windows.exe'
+  checksum      = '22ae46c8fbceaaee95f56fe12ca222fb7f86e52bd640540b9ecc061a4a31a19d'
+  checksumType  = 'sha256'
+
+  silentArgs    = '-q'
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
--- automatic/asciidocfx.install/update.ps1
+++ automatic/asciidocfx.install/update.ps1
@@ -1,6 +1,10 @@
 ﻿import-module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate {
+    Remove-Item "$PSScriptRoot\tools\*.exe"    
+    $Latest.ChecksumType32 = 'sha256'
+    $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
+}
 
 function global:au_GetLatest {
     $github_repository = "asciidocfx/AsciidocFX"
@@ -18,16 +22,13 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
-        }        
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -NoCheckUrl -ChecksumFor None
 }
