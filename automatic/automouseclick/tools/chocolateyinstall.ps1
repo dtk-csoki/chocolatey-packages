509ce20a5e1edda931c0ebbdcfe1ae3a7a8fa35e
Chocolatey <chocolatey@realdimensions.net>
2022-09-15 00:25:44 +0000

    AU: 3 updated - automouseclick embree embree.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/72198e4acae6f601835d311417556f2c688da75e

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '9E18B292D97A95249428D278FC7A553C546498E030E5CF42D9DC42B39F92CB58'
+  checksum     = '5E4A0D4D1B21A47F4D0A7FF020E3BDEDFAF8E0A10D655000AA1689F0F0431219'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
