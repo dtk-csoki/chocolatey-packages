c91d83faa359ec735a304eb768ff2d83b6b8099e
Chocolatey <chocolatey@realdimensions.net>
2022-09-30 00:26:41 +0000

    AU: 4 updated - automouseclick dontsleep dontsleep.install dontsleep.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/56c2a0a038c52220f1c0dadcdfa6574a487b2d0a

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '46142B55B6800E0BD39AEC3F865C7E5D24B1D83BB9DC02E5AFBBAD6E3454002F'
+  checksum     = 'F5F7D7D2B0D31831E562B4A5ED541DABF4DFE29D7DFCE9D50198CD40DFD580F1'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
