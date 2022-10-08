c733a29f10ce96e31ce144e020ad883641e8b5ef
Chocolatey <chocolatey@realdimensions.net>
2022-10-08 00:26:31 +0000

    AU: 1 updated - gridtracker [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/a66e1d6cbfb1c09dbad21011a313f207a2e94a44

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://storage.googleapis.com/gt_download/v1.22.1006/GridTracker-Installer.1.22.1006.exe'
-  checksum     = '1e3c983213eaa97e089afd3a57633aa9e38ddbd160edcda01d168f0ab9204f46'
+  url          = 'https://storage.googleapis.com/gt_download/v1.22.1007/GridTracker-Installer.1.22.1007.exe'
+  checksum     = '68c85c33c55e06d24ca50fc871856fd1ac9fc77396074664e9c033e686a558c6'
   checksumType = 'sha256'
 
   silentArgs   = '/S'
