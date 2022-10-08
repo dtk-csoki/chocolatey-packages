956d862ea672eb11287d1f9f9ddccfca18a13aca
chtof <chtof62@gmail.com>
2022-10-08 11:27:48 +0200

    Update chocolateyuninstall.ps1

--- automatic/locale-emulator/tools/chocolateyuninstall.ps1
+++ automatic/locale-emulator/tools/chocolateyuninstall.ps1
@@ -3,7 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Remove registry changes.
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyuninstall.ahk`""
-Start-ChocolateyProcessAsAdmin -ExeToRun $toolsDir\Locale.Emulator.2.4.1.0\LEinstaller.exe
+$targetPath = (Get-ChildItem -Path "$toolsDir" -Filter "LEinstaller.exe").FullName
+Start-ChocolateyProcessAsAdmin -ExeToRun "$targetPath"
 
 # Remove link in start menu.
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
