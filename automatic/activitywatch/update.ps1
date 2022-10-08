0439d56acae5ce90b0cadc43968bbeec965a8974
chtof <chtof62@gmail.com>
2022-10-08 12:38:24 +0200

    Update update.ps1

--- automatic/activitywatch/update.ps1
+++ automatic/activitywatch/update.ps1
@@ -1,12 +1,15 @@
 import-module au
 
 function global:au_GetLatest {	
-     $github_repository = "ActivityWatch/activitywatch"
-     $releases = "https://github.com/" + $github_repository + "/releases"
+    $github_repository = 'ActivityWatch/activitywatch'
+    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
+    $regex = "/tag/v(?<Version>.*)"
+    (Get-RedirectedUrl $releases) -match $regex | Out-Null
+    $expended_assets = 'https://github.com/' + $github_repository + '/releases/expanded_assets/v' + $matches.Version
 
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri $expended_assets -UseBasicParsing
 	$regex   = $github_repository + '/releases/download/.*/activitywatch[-_]v(?<Version>.*)-windows-x86_64.zip'
-	$url = $download_page.links | ? href -match $regex | Select -First 1
+    $url = $download_page.links | ? href -match $regex | Select -First 1
 	
      return @{ 
        Version = $matches.Version -Replace 'b','-beta'
