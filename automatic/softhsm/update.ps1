5d0056c528b04a2725469c05e54eed9a59bf76cd
chtof <chtof62@gmail.com>
2022-10-08 14:48:49 +0200

    Update update.ps1

--- automatic/softhsm/update.ps1
+++ automatic/softhsm/update.ps1
@@ -1,20 +1,9 @@
-import-module au
-
-function global:au_GetLatest {
-    $github_repository = "disig/SoftHSM2-for-Windows"
-    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex             = 'SoftHSM2-(?<Version>[\d\.]+).msi$'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match $regex
-
-    return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
-}
+. $PSScriptRoot\..\softhsm.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
