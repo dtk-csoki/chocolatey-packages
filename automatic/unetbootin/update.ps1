d3683b5ee728594ffe42d434a8c35e8e7cb3bbca
chtof <chtof62@gmail.com>
2022-10-08 13:29:11 +0200

    Update update.ps1

--- automatic/unetbootin/update.ps1
+++ automatic/unetbootin/update.ps1
@@ -1,20 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
+. ..\..\helpers\GitHub_Helper.ps1
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'unetbootin/unetbootin'
+        regex32    = 'unetbootin-windows-(?<Version>.*).exe'        
+   }
+}
 
 function global:au_BeforeUpdate {
     $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
-}
-
-function global:au_GetLatest {
-    $github_repository = "unetbootin/unetbootin"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex   = $github_repository + '/releases/download/.*/unetbootin-windows-(?<Version>.*).exe'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url = $download_page.links | ? href -match $regex
-	$version = $matches.Version -replace '\w','$&.' -replace '\.$', ''
-    
-    return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
 }
 
 function global:au_SearchReplace {
