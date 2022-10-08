2ba1616e11c59741f82116274a1690f4f81e473f
chtof <chtof62@gmail.com>
2022-10-08 14:45:07 +0200

    Update update.ps1

--- automatic/spacemacs/update.ps1
+++ automatic/spacemacs/update.ps1
@@ -1,19 +1,13 @@
 ﻿import-module au
+. ..\..\helpers\GitHub_Helper.ps1
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
-function global:au_GetLatest {	
-    $github_repository = "syl20bnr/spacemacs"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    # $regex   = "/archive/v(?<Version>[\d\.]+).zip$"
-    $regex   = "/v(?<Version>[\d\.]+).zip$"
-
-	$url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
-
-	return @{
-		Version = $matches.Version
-		URL32 = "https://github.com" + $url.href
-	}
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'syl20bnr/spacemacs'
+        regex32    = '/v(?<Version>[\d\.]+).zip$'        
+   }
 }
 
 function global:au_SearchReplace {
@@ -32,6 +26,4 @@ function global:au_SearchReplace {
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none
