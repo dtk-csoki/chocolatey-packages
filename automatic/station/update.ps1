88a136349a6a8e0e191a62b3a6155a1938aaea95
chtof <chtof62@gmail.com>
2022-10-08 09:44:45 +0200

    Create update.ps1

---
+++ automatic/station/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://github.com/getstation/desktop-app/releases/latest'
+    # $regex   = 'browserX-Setup-(?<Version>[\d\.]+).exe$'    
+    $regex   = '/releases/tag/v(?<Version>[\d\.]+)'
+    (Get-RedirectedUrl $releases) -match $regex
+
+    return @{
+        Version = $matches.version
+        URL32 = 'https://github.com/getstation/desktop-app/releases/download/v' + $matches.Version + '/Station-Setup.exe'        
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
