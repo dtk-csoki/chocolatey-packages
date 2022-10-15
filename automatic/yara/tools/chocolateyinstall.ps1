8d76e55c60171aca668e4f30c967a18610e2ea8f
Chocolatey <chocolatey@realdimensions.net>
2022-10-15 18:06:51 +0000

    AU: 2 updated - cudatext yara [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b5f8e7727ae4a843b5416e1f3b5156cfd3946ccd

--- automatic/yara/tools/chocolateyinstall.ps1
+++ automatic/yara/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\yara-v4.2.2-2012-win32.zip"
-  file64      = "$toolsDir\yara-v4.2.2-2012-win64.zip"
+  file        = "$toolsDir\yara-4.2.3-2029-win32.zip"
+  file64      = "$toolsDir\yara-4.2.3-2029-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
