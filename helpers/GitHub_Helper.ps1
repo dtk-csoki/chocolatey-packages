09cd688f78b9c0e00938b451212504485fc9e425
chtof <chtof62@gmail.com>
2022-10-15 20:00:19 +0200

    Update GitHub_Helper.ps1

--- helpers/GitHub_Helper.ps1
+++ helpers/GitHub_Helper.ps1
@@ -5,34 +5,32 @@ function github_GetInfo {
     $github_url = 'https://github.com/'
     $github_repository_root = "$github_url" + $ArgumentList.repository
 
-    If ($debug) { Write-Host "`$github_redirected_url  = (Get-RedirectedUrl "${github_repository_root}/releases/latest")" }
-    $github_redirected_url  = (Get-RedirectedUrl "${github_repository_root}/releases/latest")
+    If ($debug) { Set-PsDebug -Trace 1 }
+
+    $github_redirected_url  = Get-RedirectedUrl "${github_repository_root}/releases/latest"
     # v is optional (ex: https://github.com/adobe-fonts/source-han-code-jp/releases/latest)
     $github_latest_version  = "$github_redirected_url" -match '/tag/(?<FullVersion>v?(?<Version>[\d\.]*).*)' # Get version to re-use for expanded_assets
 
     $github_expanded_assets = "$github_repository_root" + '/releases/expanded_assets/' + $matches.FullVersion
     $isVersionMatched = $false
 
+    $version = $matches.Version
     $regex32 = $ArgumentList.regex32;
     $regex64 = $ArgumentList.regex64;
     $output = @{}
 
-    If ($debug) { Write-Host $matches.Version }    
-    If (-Not([string]::IsNullOrEmpty($matches.Version))) {
+    If (-Not([string]::IsNullOrEmpty($version))) {
         # If version has not the correct format - Ex: 2.0.12R -> 2.0.12 (source-han-code-jp)
-        If ($matches.Version -Notmatch "^[\d\.]+$") {
-            $matches.Version = $matches.Version -replace '([\d\.]+).*', '$1'
+        If ($version -NotMatch "^[\d\.]+$") {
+            $version = $version -replace '([\d\.]+).*', '$1'
         } 
-
-        If ($debug) { Write-Host "`$download_page = Invoke-WebRequest -Uri $github_expanded_assets -UseBasicParsing" }
-        $download_page = Invoke-WebRequest -Uri $github_expanded_assets -UseBasicParsing    
+        $download_page = Invoke-WebRequest -Uri $github_expanded_assets -UseBasicParsing        
     } Else {
         # If "${github_repository_root}/releases/latest" does not redirect to an URL like '/tag/v(?<Version>.*)'
         # Ex: zVirtualDesktop - <h1 data-view-component="true" class="d-inline mr-3">1.0.98.14</h1>        
         $download_page = Invoke-WebRequest -Uri $github_redirected_url -UseBasicParsing        
         $version = $download_page -match '<h1 data-view-component="true" class="d-inline mr-3">(?<Version>[\d\.]+)<'
     }
-    $version = $matches.Version
 
     If ($regex64) {
         $uri64_path = ($download_page.links | ? href -match $regex64 | select -Last 1).href
@@ -55,10 +53,10 @@ function github_GetInfo {
             $version = $matches.Version -replace "([\d\.]+).*", '$1'
         }
     }
-    If ($regex32) {
+    If ($regex32) {            
         $uri32_path = ($download_page.links | ? href -match $regex32 | select -Last 1).href
 
-        If ($uri32_path -eq $null) {            
+        If ($uri32_path -eq $null) {        
             $uri32_path = $download_page.Links.href -match $regex32
         }
 
@@ -81,11 +79,12 @@ function github_GetInfo {
 
     If ($debug) {
         Write-Host "github_releases_latest:" "${github_repository_root}/releases/latest"
+        Write-Host "github_redirected_url:" $github_redirected_url
         Write-Host "github_latest_version:" $github_latest_version
-        Write-Host "github_expanded_assets:" $github_expanded_assets
+        Write-Host "github_expanded_assets:" $github_expanded_assets        
         Write-Host "Regex32:" $ArgumentList.regex32
         Write-Host "Regex64:" $ArgumentList.regex64
-        Write-Host "Version:" $matches.Version
+        Write-Host "Version:" $output.Version
         Write-Host "uri32_path: " $uri32_path
         Write-Host "uri64_path: " $uri64_path
         Write-Host "output: " ($output | Format-Table | Out-String)
@@ -93,3 +92,5 @@ function github_GetInfo {
 
     return $output
 }
+
+Set-PsDebug -Trace 0
