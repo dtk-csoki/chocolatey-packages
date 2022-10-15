6f3870ad2e91d1a0e190494cc25245a8960fc20c
chtof <chtof62@gmail.com>
2022-10-15 14:15:02 +0200

    Update update.ps1

--- automatic/zvirtualdesktop/update.ps1
+++ automatic/zvirtualdesktop/update.ps1
@@ -1,5 +1,14 @@
 import-module au
+. ..\..\helpers\GitHub_Helper.ps1
 
+#function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'mzomparelli/zVirtualDesktop'        
+        regex32 = 'https://zomp.co/Files.aspx\?id='
+   }
+}
 
 function global:au_BeforeUpdate {
     Remove-Item "$PSScriptRoot\tools\*.exe"
@@ -8,21 +17,6 @@ function global:au_BeforeUpdate {
 
     $Latest.ChecksumType = "sha256"
     $Latest.Checksum32   = Get-FileHash -Algorithm $Latest.ChecksumType -Path "${env:tmp}\zVirtualDesktop.exe" | ForEach-Object Hash
-}
-
-function global:au_GetLatest {
-    $github_repository = 'mzomparelli/zVirtualDesktop'
-    $releases      = "https://github.com/" + $github_repository + "/releases/latest"    
-    #$regex = "zVirtualDesktop.exe"
-    $regex = "https://zomp.co/Files.aspx\?id="
-    $regex_version = '/tag/(?<Version>[\d\.]+)'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url = ($download_page).links | ? href -match $regex
-    $download_page -match $regex_version
-    $version = $matches.Version
-        
-	return @{ Version = $version ; URL32 = $url.href }
 }
 
 function global:au_SearchReplace {
