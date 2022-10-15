910e73709ac5cabc5995aef9b10af312593377f7
chtof <chtof62@gmail.com>
2022-10-15 12:04:37 +0200

    Update update.ps1

--- automatic/tusk/update.ps1
+++ automatic/tusk/update.ps1
@@ -1,15 +1,4 @@
-import-module au
-
-function global:au_GetLatest {
-    $github_repository = "klaussinani/tusk"
-    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex             = 'tusk-setup-(?<Version>[\d\.]+).exe$'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match $regex
-
-    return @{ Version = $matches.Version }
-}
+. $PSScriptRoot\..\tusk.install\update.ps1
 
 function global:au_SearchReplace {
    @{
