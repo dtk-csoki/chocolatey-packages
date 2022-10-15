f32bf7ade86762d0bd4d8f37546ff0c1cd740517
Chocolatey <chocolatey@realdimensions.net>
2022-10-15 16:26:27 +0000

    AU: 1 updated - zvirtualdesktop [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/738127eef6f06b9b098c47b1370cdd7446e3afc6

--- automatic/zvirtualdesktop/tools/chocolateyinstall.ps1
+++ automatic/zvirtualdesktop/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   file          = "$toolsDir\zVirtualDesktop.exe"
 
   url           = 'https://zomp.co/Files.aspx?id=zVDEXE'
-  checksum      = 'D141495352FE95F471303BA33C17F1FBE9122D2C80F7855CCE29220D5611FF1C'
+  checksum      = '748D3C614CE880B186550BF473148C0316512255520C66E6B4E688F33E7019A9'
   checksumType  = 'sha256'
 }
 
