b63e9875bf8fced58fbf369eeb2eb09b153b9956
Chocolatey <chocolatey@realdimensions.net>
2022-10-21 00:26:19 +0000

    AU: 3 updated - automouseclick keeper seppdf [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/145c353359e46bfc58170fc2cbd302cd6f912650

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'C3F06E355F0EB40D2CAE984B519AAABBAC1D649417F8F21ADD18CCEAC1A1D6F4'
+  checksum     = 'BB67EF7D958B7AEBB954BD9776552AFC68D48B7E665BB81982BF8A90B2160497'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
