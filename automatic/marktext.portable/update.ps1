82879c50930d29fed7dc3f1d6402ab6de85e1f6d
chtof <chtof62@gmail.com>
2022-10-22 23:22:01 +0200

    Update update.ps1

--- automatic/marktext.portable/update.ps1
+++ automatic/marktext.portable/update.ps1
@@ -1,25 +1,17 @@
 ﻿import-module au
+. ..\..\helpers\GitHub_Helper.ps1
 
 function global:au_BeforeUpdate() {
   $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
   $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
-function global:au_GetLatest {	
-    $github_repository = 'marktext/marktext'
-    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
-    $regex32  = '/.*-ia32-win.zip'
-    $regex64  = '/v(?<Version>[\d\.]*)/.*-x64-win.zip'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
-	$url32 = $download_page.links | ? href -match $regex32
-    $url64 = $download_page.links | ? href -match $regex64
-
-    return @{
-        Version = $matches.Version
-        URL32 = 'https://github.com' + $url32.href
-        URL64 = 'https://github.com' + $url64.href
-    }
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'marktext/marktext'
+        regex32    = '/.*-ia32-win.zip'
+        regex64    = '/v(?<Version>[\d\.]*)/.*-x64-win.zip'
+   }
 }
 
 function global:au_SearchReplace {
