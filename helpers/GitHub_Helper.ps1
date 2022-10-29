b948cb13e50f9eeaffd7f66aad104c6bcc0bb961
chtof <chtof62@gmail.com>
2022-10-29 17:03:39 +0200

    Update GitHub_Helper.ps1

--- helpers/GitHub_Helper.ps1
+++ helpers/GitHub_Helper.ps1
@@ -8,6 +8,8 @@ function github_GetInfo {
     If ($debug) { Set-PsDebug -Trace 1 }
 
     $github_redirected_url  = Get-RedirectedUrl "${github_repository_root}/releases/latest"
+    
+    Write-Host "github_redirected_url: $github_redirected_url"
 
     # If "${github_repository_root}/releases/latest" redirect to a URL like '/tag/(?<FullVersion>v?(?<Version>[\d\.]*).*)'
     If ($github_redirected_url -match '/tag/v?[\d\.]*.*') {
@@ -19,6 +21,8 @@ function github_GetInfo {
     }
 
     $github_expanded_assets = "$github_repository_root" + '/releases/expanded_assets/' + $matches.FullVersion
+    Write-Host "github_expanded_assets: $github_expanded_assets"
+
     $isVersionMatched = $false
 
     # "2022-03-08" to "2022.03.08" - Exp: https://github.com/fontforge/fontforge/releases/tag/20220308
@@ -27,28 +31,37 @@ function github_GetInfo {
     $regex64 = $ArgumentList.regex64;
     $output = @{}
 
-    If (-Not([string]::IsNullOrEmpty($version))) {
+    # If Version is not null or empty
+    <#If (-Not([string]::IsNullOrEmpty($version))) {
         # If version in the $github_expanded_assets URL has not the correct format - Ex: 2.0.12R -> 2.0.12 (source-han-code-jp)
         If ($version -NotMatch "^[\d\.]+$") {
             $version = $version -replace '([\d\.]+).*', '$1'
         }        
         $download_page = Invoke-WebRequest -Uri $github_expanded_assets -UseBasicParsing
-        $used_page = $github_expanded_assets
+        
     } Else {
         # If "${github_repository_root}/releases/latest" does not redirect to an URL like '/tag/v(?<Version>.*)'
         # Ex: zVirtualDesktop - <h1 data-view-component="true" class="d-inline mr-3">1.0.98.14</h1>        
         $download_page = Invoke-WebRequest -Uri $github_redirected_url -UseBasicParsing
-        $used_page = $github_redirected_url
+        
         $version = $download_page -match '<h1 data-view-component="true" class="d-inline mr-3">(?<Version>[\d\.]+)<'
-    }
+    }#>
+
+    $download_page = Invoke-WebRequest -Uri $github_expanded_assets -UseBasicParsing
+    $used_page = $github_expanded_assets
 
     If ($regex64) {
         $uri64_path = ($download_page.links | ? href -match $regex64 | select -Last 1).href
 
         If ([string]::IsNullOrEmpty($uri64_path)) {
             $uri64_path = $download_page.Links.href -match $regex64
+            # If regex64 unavailable on $github_expanded_assets URL, we try on $github_redirected_url
             If ([string]::IsNullOrEmpty($uri64_path)) {
-                Write-Error "$regex64 not found in links displayed on $used_page" -ErrorAction Stop
+                $download_page = Invoke-WebRequest -Uri $github_redirected_url -UseBasicParsing
+                $uri64_path = $download_page.Links.href -match $regex64
+                If ([string]::IsNullOrEmpty($uri64_path)) {
+                    Write-Error "$regex64 not found in links displayed on $github_expanded_assets and $github_redirected_url" -ErrorAction Stop
+                }
             }
         }
 
@@ -73,13 +86,19 @@ function github_GetInfo {
             }
         }
     }
+
     If ($regex32) {            
         $uri32_path = ($download_page.links | ? href -match $regex32 | select -Last 1).href
 
         If ([string]::IsNullOrEmpty($uri32_path)) {            
             $uri32_path = $download_page.Links.href -match $regex32
+            # If regex32 unavailable on $github_expanded_assets URL, we try on $github_redirected_url
             If ([string]::IsNullOrEmpty($uri32_path)) {
-                Write-Error "$regex32 not found in links displayed on $used_page" -ErrorAction Stop
+                $download_page = Invoke-WebRequest -Uri $github_redirected_url -UseBasicParsing
+                $uri32_path = $download_page.Links.href -match $regex32
+                If ([string]::IsNullOrEmpty($uri32_path)) {
+                    Write-Error "$regex32 not found in links displayed on $github_expanded_assets and $github_redirected_url" -ErrorAction Stop
+                }
             }
         }
 
@@ -107,8 +126,8 @@ function github_GetInfo {
         }
     }
 
-    If (-Not($output.Version)) { Write-Warning "Version is missing."}
-    If ((-Not($output.URL32))-And(-Not($output.URL64))) { Write-Warning "URL32 and/or URL64 are missing."}
+    If (-Not($output.Version)) { Write-Error "Version is missing." -ErrorAction Stop }
+    If ((-Not($output.URL32))-And(-Not($output.URL64))) { Write-Error "URL32 and/or URL64 are missing." -ErrorAction Stop }
 
     If ($debug) {
         Write-Host "github_releases_latest:" "${github_repository_root}/releases/latest"
@@ -126,4 +145,4 @@ function github_GetInfo {
     return $output
 }
 
-Set-PsDebug -Trace 0
+#Set-PsDebug -Trace 0
