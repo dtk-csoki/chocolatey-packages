7d6f3cb6377ce18575cc182c8eaecd64961a8e19
chtof <chtof62@gmail.com>
2022-10-29 12:45:52 +0200

    Update update.ps1

--- automatic/clavier-plus.portable/update.ps1
+++ automatic/clavier-plus.portable/update.ps1
@@ -1,29 +1,17 @@
 ﻿import-module au
 
+. ..\..\helpers\GitHub_Helper.ps1
+
 function global:au_BeforeUpdate {
     Get-RemoteFiles -NoSuffix -Purge
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 function global:au_GetLatest {
-   $github     = 'https://github.com/'
-    $repository = 'guilryder/clavier-plus'
-
-    $github_repository = $github + $repository
-    $releases = $github_repository + '/releases/latest'
-    $regexVersion = '/tree/release(?<Version>[\d\.]+)'
-    $regexUrl = 'Clavier.zip'
-
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing) -match $regexVersion | Out-Null
-    $version = $matches.Version
-    $path = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regexUrl).href
-
-    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null
-
-     return @{
-        Version = $version
-        URL64   = 'https://github.com' + $path
-    }
+   return github_GetInfo -ArgumentList @{
+        repository = 'guilryder/clavier-plus'
+        regex64    = '/download/release(?<Version>[\d\.]+)/Clavier.zip'
+   }
 }
 
 function global:au_SearchReplace {
