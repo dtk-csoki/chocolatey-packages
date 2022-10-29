f582ab2132f6805f8748df0e2c67689b183bf756
chtof <chtof62@gmail.com>
2022-10-29 19:06:05 +0200

    Update update.ps1

--- automatic/unetbootin/update.ps1
+++ automatic/unetbootin/update.ps1
@@ -3,10 +3,16 @@ import-module au
 . ..\..\helpers\GitHub_Helper.ps1
 
 function global:au_GetLatest {
-   return github_GetInfo -ArgumentList @{
+    $output = github_GetInfo -ArgumentList @{
         repository = 'unetbootin/unetbootin'
         regex32    = 'unetbootin-windows-(?<Version>.*).exe'        
-   }
+    }
+    If ($output.Version -match '\d{3}') {
+        $output.Version = $output.Version -replace '(\d)(\d)(\d)', '$1.$2.$3'
+    } ElseIf ($output.Version -match '\d{3}') {
+        $output.Version = $output.Version -replace '(\d)(\d)(\d)(\d)', '$1.$2.$3.$4'
+    }
+    return $output
 }
 
 function global:au_BeforeUpdate {
