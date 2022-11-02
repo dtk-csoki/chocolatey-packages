98887362e9706197292f69b49fcad8292b837b06
Chocolatey <chocolatey@realdimensions.net>
2022-11-02 00:25:36 +0000

    AU: 4 updated - klayout openssl sublimemerge webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/eba29edc762df479d6041eb62ef3e8fe798d6474

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url64         = 'https://download.sublimetext.com/sublime_merge_build_2077_x64_setup.exe'
-  checksum64    = 'dfd6cd698c598c816dca5b43602288095263d5db17a000fbd0b3dc934f7e876d'
+  url64         = 'https://download.sublimetext.com/sublime_merge_build_2078_x64_setup.exe'
+  checksum64    = 'c0cf1f462af422853dce7edb1009134a4f64080a1fec1cf20fb08da7997ceb04'
   checksumType  = 'sha256'
   
   
