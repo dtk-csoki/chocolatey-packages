69882221942a19628485744754af393be1627eab
Chocolatey <chocolatey@realdimensions.net>
2022-11-03 08:25:10 +0000

    AU: 4 updated - keeper moonlight-qt moonlight-qt.install moonlight-qt.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/4faa63abe039e4576123dec742b0d8bf67333263

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = 'ce454d8e1ee68a839edda8a7eb1e6c51c67d1039e67a25ece3e623714aabaa24'
+  checksum      = '74272d8e32e5012b0e0adca2fc0b6a352678fba0fde5224f2fae2cf9abcb3e18'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '2a398b9b7dee844990408eab428b37b3d7c10067d2b29f2697643393e5988265'
+  checksum64    = 'eeb3890b3a18f3b3ed8b52851f8bb50e55e917b09991e6c74c834a4e3c2322f6'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
