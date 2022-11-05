17bcef632009c050946c3d715a6a23ff5957bd15
chtof <chtof62@gmail.com>
2022-11-05 10:46:47 +0100

    Create update.ps1

---
+++ manual/vagrant-manager/update.ps1
@@ -0,0 +1,22 @@
+import-module au
+. ..\..\helpers\GitHub_Helper.ps1
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'lanayotech/vagrant-manager-windows'
+        regex32    = '/releases/download/.*/VagrantManager-(?<Version>[\d\.]+)-Setup.exe$'        
+   }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update
