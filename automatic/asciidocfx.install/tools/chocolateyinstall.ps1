647476da85d33a47b6483b19f6f991bda19dcbc6
Chocolatey <chocolatey@realdimensions.net>
2022-11-07 00:25:34 +0000

    AU: 3 updated - asciidocfx asciidocfx.install asciidocfx.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/59c7d0a5af0e3b507c984084013cd8a0614c7166

--- automatic/asciidocfx.install/tools/chocolateyinstall.ps1
+++ automatic/asciidocfx.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  url           = 'https://github.com/asciidocfx/AsciidocFX/releases/download/v1.8.1/AsciidocFX_Windows.exe'
-  checksum      = '9d32a01a8d16fee9b02e4ad0ab4fd36eb0bd3c9f78e0ac3176d544f799baa4d9'
+  url           = 'https://github.com/asciidocfx/AsciidocFX/releases/download/v1.8.2/AsciidocFX_Windows.exe'
+  checksum      = '738413d42a8ddf03288b2919e4885f9dc83122691b2ec352d1bb00ac65c3be7a'
   checksumType  = 'sha256'
 
   silentArgs    = '-q'
