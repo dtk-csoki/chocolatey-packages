7c48451ac2f92d80912901b97fe181d72ba4338b
Chocolatey <chocolatey@realdimensions.net>
2022-11-08 16:24:57 +0000

    AU: 3 updated - gns3 kmymoney rapidminer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/3b3510a36c31c7b5c71df75c79a4d039f2486797

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1580-windows-msvc2019_64-cl.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1581-windows-msvc2019_64-cl.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'b64148860daaef77ea3a825577314ff9d6a8eef82ae136d564a794858c53aa54'
+  checksum64     = '7669c02d28ec946746089e65cdf9e7ea6ce28b56dc5dbb1e4d16f25203760aa4'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/rapidminer/tools/chocolateyinstall.ps1
+++ automatic/rapidminer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
    
   url           = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win32-install.exe?utm_source=nexus&utm_medium=nexus'
-  checksum      = 'c80a0a35566c3af74080114eb0f70741765a17240e7083668e004c8c2b25615a'
+  checksum      = 'f523de4b1a15f86b0097ab11ecacef907251da399ed45591fc716423257d47e8'
   checksumType  = 'sha256'
 
   url64bit      = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win64-install.exe?utm_source=nexus&utm_medium=nexus'
-  checksum64    = 'b3e2cea2ecd1bb6b0bb9f9d1e39915207739c8778c05f80d0cd968ad0aeba54a'
+  checksum64    = '90341db72a25763832d754f57593093cf35ce183df24e387ab3957943f6556ff'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
