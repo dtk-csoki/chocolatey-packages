02248487b5a4b84dda979f6f20910c532165dcf3
chtof <chtof62@gmail.com>
2022-11-11 21:06:00 +0100

    Create _update.ps1

---
+++ automatic/elmer/_update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {    
+    $releases = 'https://www.nic.funet.fi/pub/sci/physics/elmer/bin/windows/'
+
+    #$regex    = 'embree-(?<Version>[\d\.]+).x64.vc[\d]+(.windows)?\.zip$'
+
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1
+
+    return @{
+        Version = $matches.Version
+        URL64   = "https://github.com" + $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"  = @{
+            "(?i)(x64: ).*"       = "`${1}$($Latest.URL64)"
+            "(?i)(checksum64:).*" = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}
