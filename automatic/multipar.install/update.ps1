1f3bca4cfd6db8f29d7f2761422e7b61b7678104
chtof <chtof62@gmail.com>
2022-11-11 23:56:38 +0100

    Update update.ps1

--- automatic/multipar.install/update.ps1
+++ automatic/multipar.install/update.ps1
@@ -6,27 +6,25 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'Yutaka-Sawada/MultiPar'        
-        regex64    = '/(?<File>MultiPar[\d]+_setup.exe)'
+        regex64    = '/MultiPar[\d]+_setup.exe$'        
    }
 }
 
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor None
 }
