50ec3c1d7c8461731d7689b88ae6285cea1fa9ea
chtof <chtof62@gmail.com>
2022-11-11 21:06:11 +0100

    Update chocolateyinstall.ps1

--- automatic/elsie/tools/chocolateyinstall.ps1
+++ automatic/elsie/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url			= 'http://tonnesoftware.com/downloads/LCinstall284.exe'
-  checksum		= 'EDC68F0E87D94A50B06EBD890E8E6BDEFD9F23B672801DF2F40B1E3B28A4AEBC'
+  url			= 'http://tonnesoftware.com/downloads/LCinstall286.exe'
+  checksum		= '6dcc47cf379b2b39377833bb379028c7d17ba3c4b11878553637f72a39407f2c'
   checksumType	= 'sha256'
 
   validExitCodes= @(1)
