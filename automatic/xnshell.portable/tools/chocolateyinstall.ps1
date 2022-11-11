b456ce93382e4047385e52acb53a75e6eb09fadb
chtof <chtof62@gmail.com>
2022-11-11 21:14:44 +0100

    Update chocolateyinstall.ps1

--- automatic/xnshell.portable/tools/chocolateyinstall.ps1
+++ automatic/xnshell.portable/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   checksum      = 'E17DEFAEB3A5E1C081B7EB27AACF049DD07CBC48DF63477904AAA571037EA0E0'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://download.xnview.com/XnShellEx64.zip'
+  url64         = 'https://download.xnview.com/XnShellEx64.zip'
   checksum64    = 'F4041EFD70B95EC53ADA9A6006A7565F92F2DCBFA2995F7030E2FCD91920E0BA'
   checksumType64= 'sha256'
 }
