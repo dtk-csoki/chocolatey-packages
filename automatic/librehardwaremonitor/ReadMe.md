8c96b32da921ad9c7b22f2c1b203c82917417559
chtof <chtof62@gmail.com>
2022-11-11 23:09:37 +0100

    librehardwaremonitor

---
+++ automatic/librehardwaremonitor/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/libre-hardware-monitor/librehardwaremonitor.png" width="48" height="48"/> [LibreHardwareMonitor](https://chocolatey.org/packages/librehardwaremonitor)
+
+Libre Hardware Monitor, fork of Open Hardware Monitor.
---
+++ automatic/librehardwaremonitor/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\LibreHardwareMonitor-net472.zip"  
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Libre Hardware Monitor.lnk"
+$targetPath = Join-Path $toolsDir "LibreHardwareMonitor.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
