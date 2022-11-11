6dbcf373a2032aab81fae585b097cbc7417223dc
chtof <chtof62@gmail.com>
2022-11-11 21:18:06 +0100

    Update chocolateyinstall.ps1

--- automatic/xnshell.install/tools/chocolateyinstall.ps1
+++ automatic/xnshell.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://download.xnview.com/XnShellEx.exe'
-  checksum      = 'A1356FE3BA6ED7C1914B970CCC87AB2A77403866A9E677321633032084A038B3'
+  checksum      = 'CC7872F775A5754B2BAA763583B594D2A110822A87CB8DD731B20E422AC8E73B'
   checksumType  = 'sha256'
 
   url64         = 'https://download.xnview.com/XnShellEx64.exe'
-  checksum64    = '9A4EAB980C441402EE5B040910110A61150A79A1F745E68002B9BE30ECCB9068'
+  checksum64    = 'C82F9494C867C0E680CC8DEF364DE6F43BD84F2466B99181EB48633A69A9703E'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
