22af0b4084bc0c3d9de4623d4a8e2fbb018ef8da
Chocolatey <chocolatey@realdimensions.net>
2022-11-18 00:24:56 +0000

    AU: 2 updated - cudatext seppdf [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e3e6c6b2947ff1c5a1b782f6fd170079f94916f7

--- automatic/seppdf/tools/chocolateyinstall.ps1
+++ automatic/seppdf/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir" 
 
-  url           = 'https://www.ne.jp/asahi/foresth/home/spdf360.zip'
-  checksum      = '0d3649d56b4aab23a027b747329b903723bf77092f56f93cf95db3652ba6d2cd'
+  url           = 'https://www.ne.jp/asahi/foresth/home/spdf361.zip'
+  checksum      = 'aca72a2a9eda2267e8f2da65c6e216df3a2dcb00994c6e680b30f10ee58a277c'
   checksumType  = 'sha256'
 }
 
