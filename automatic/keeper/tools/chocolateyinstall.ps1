986f1fc459d1116f8cde40b0b4d57501247f65e7
chtof <chtof62@gmail.com>
2022-11-18 21:01:14 +0100

    Update chocolateyinstall.ps1

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -1,30 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$toolsDir"
-  fileType      = 'EXE'
-  file          = "$toolsDir\KeeperSetup32.exe"
-  file64        = "$toolsDir\KeeperSetup64.exe"
+  AppxPackageName = 'KeeperSecurityInc.KeeperPasswordManager'
+  packageName     = $env:ChocolateyPackageName
 
-  url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '74272d8e32e5012b0e0adca2fc0b6a352678fba0fde5224f2fae2cf9abcb3e18'
-  checksumType  = 'sha256'
+  url             = 'https://www.keepersecurity.com/desktop_electron/Win32/KeeperSetup32.msi'
+  checksum        = '21b829a2eeb9ad135b073e0b4242e48cd4f07b8ce24ba1b955ad27bc16408876'
+  checksumType    = 'sha256'
 
-  url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'eeb3890b3a18f3b3ed8b52851f8bb50e55e917b09991e6c74c834a4e3c2322f6'
-  checksumType64= 'sha256'
+  url64           = 'https://www.keepersecurity.com/desktop_electron/packages/KeeperPasswordManager.msixbundle'
+  checksum64      = '7b31bb10f0ec1d39946411e812929afbff30eebb6aba62bf5661365551040111'
+  checksumType64  = 'sha256'
 
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyInstallPackage @packageArgs
+If (Get-OSArchitectureWidth -eq 64){
+  # 64 bits
+  #The .msixbundle format is not supported on Windows 10 version 1709 and 1803. https://docs.microsoft.com/en-us/windows/msix/msix-1709-and-1803-support
+  $IsCorrectBuild=[Environment]::OSVersion.Version.Build
+  If ($IsCorrectBuild -lt "18362") {
+    Throw "This package requires at least Windows 10 version 1903/OS build 18362.x."
+  }
+  Add-AppPackage -Path $packageArgs.url64
+} Else {
+  # 32 bits
+  Install-ChocolateyPackage @packageArgs
+  # Install start menu shortcut
+  $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+  $target = "$env:LOCALAPPDATA\keeperpasswordmanager"
+  $shortcutFilePath = Join-Path $programs "Keeper.lnk"
+  $targetPath = Join-Path $target "keeperpasswordmanager.exe"
+  Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+}
 
-# Install start menu shortcut
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$target = "$env:LOCALAPPDATA\keeperpasswordmanager"
-$shortcutFilePath = Join-Path $programs "Keeper.lnk"
-$targetPath = Join-Path $target "keeperpasswordmanager.exe"
-Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
