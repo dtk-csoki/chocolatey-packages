6aabb02c8e9e42429567a857081ba0b769fbb211
chtof <chtof62@gmail.com>
2022-11-19 11:00:00 +0100

    Update update.ps1

--- automatic/altcopy/update.ps1
+++ automatic/altcopy/update.ps1
@@ -1,6 +1,19 @@
 import-module au
 
 function global:au_GetLatest {
+    add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
     $releases = 'https://altcopy.net/versionPC'
     $version  = ((Invoke-WebRequest -uri $releases).RawContent.Split([Environment]::NewLine) | Select -Last 1) -Replace '\.000$', ''
 
