2ec53acea54b6a3effeeb51a4e76764d3e8d3eb6
Chocolatey <chocolatey@realdimensions.net>
2022-11-26 16:24:48 +0000

    AU: 3 updated - cudatext goodsync kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/54f3af3849a72a0806eb1a8e739b358e898cae46

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = '65fde68862663371e90aa461893d30eb1231d8bc6494b0530daa2fdb8ba1835f'
+  checksum       = '0541648de980f76a19d7b4e3f4f72a873de86b98c72b0f1eb2fa1e2905988a69'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1598-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1599-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '267822d15dc3212891071f3c4bf5765c2bd94c64c2dfc35ebbd7ab3b22f14b08'
+  checksum64     = '64b17fd7d01a3fc614cb6437de41e46c59eab6c36110a56e49ec609d536e9867'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
