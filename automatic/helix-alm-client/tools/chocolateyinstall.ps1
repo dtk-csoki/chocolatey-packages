c2036f04d83158716f543d34c091906ca6c90bcf
Chocolatey <chocolatey@realdimensions.net>
2022-12-06 00:25:00 +0000

    AU: 2 updated - helix-alm-client sublimemerge [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/74b77849f4894ec6934f3d36ca2c6d349afacd78

--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2022.2.0/ttwinclientinstall.exe'
-  checksum     = 'b9f7119051db91bb0e2a64d8b1e550ecb303c29d564ec00fb66093327f75eb25'
+  url          = 'https://cdist2.perforce.com/alm/helixalm/r2022.3.0/ttwinclientinstall.exe'
+  checksum     = '799907ecdb92797cc6223e77e0e721a85e3b4e74e80c587f88b04f67cd4ef14d'
   checksumType = 'sha256'
 
   silentArgs   = "-i SILENT LAX_VM resource\jre\bin\java.exe"
--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url64         = 'https://download.sublimetext.com/sublime_merge_build_2079_x64_setup.exe'
-  checksum64    = 'f7b34ed40aeae176e72d2bd5cc8025c7653eef7a78dc4949d8e6f66b7aa77a9f'
+  url64         = 'https://download.sublimetext.com/sublime_merge_build_2080_x64_setup.exe'
+  checksum64    = '0a21d96fd9d4e275009ece5ab62c4a2bb06cffa57444843f2cffa4b4892c9b88'
   checksumType  = 'sha256'
   
   
