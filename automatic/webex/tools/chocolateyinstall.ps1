baa1bcb336f6db36a0d9a8a6031046e25f0ea287
Chocolatey <chocolatey@realdimensions.net>
2022-12-06 16:25:12 +0000

    AU: 1 updated - webex [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/043c6fc74ac7f5592a4415dcf1dca0b051bbb009

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -17,11 +17,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = 'A46BF30C3289B1C69FF3943D8E8BD66D9B063BA3523AFBBFFBD9856CC18F7823'
+  checksum      = '86EC2711412474C636B14B3CB3976CB14C6F395C357B1D9DA2143CA1D9F1B0A1'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = 'F0849A6D9422A5A4CD662C6417525A1A6FC5D7F9CAC4CBF3145A11D3CE4B4ADE'
+  checksum64    = 'B3FBAA88AFF51AC0FF50FDA951B888954AA194B2F7A4A8E54C335C2CCD572402'
   checksumType64= 'sha256'
 
   silentArgs    = "$sa /qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
