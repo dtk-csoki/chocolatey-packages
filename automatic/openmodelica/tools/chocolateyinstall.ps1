762c8f8ff8b72cfbba8a22ad027867e5d62df22d
Chocolatey <chocolatey@realdimensions.net>
2022-12-08 16:27:00 +0000

    AU: 4 updated - openmodelica phonerlite phonerlite.install phonerlite.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/0b2f98205b2339013d0e5133d9fd790e1d6901c7

--- automatic/openmodelica/tools/chocolateyinstall.ps1
+++ automatic/openmodelica/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.19/2/32bit/OpenModelica-v1.19.2-32bit.exe'
-  checksum      = 'bca175a6c31ff1bef067af42255b4c50107455ed06cb405aae7e212a10f6d5ad'
+  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.20/0/32bit/OpenModelica-v1.20.0-32bit.exe'
+  checksum      = '5874c035e95e73eb1ff935c63d9c0a7b57126fded8220c4fc22aed2892cb10c8'
   checksumType  = 'sha256'
   
-  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.19/2/64bit/OpenModelica-v1.19.2-64bit.exe'
-  checksum64    = '2785f39376914f8514588ee3cf1f449b57546a4b321b769fb2d46336a00adbec'
+  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.20/0/64bit/OpenModelica-v1.20.0-64bit.exe'
+  checksum64    = '3e8fd7f4bd7f0f6edebfa504b89acfc5caffdcceecba20b92d1d43de845d5d12'
   checksumType64= 'sha256'
   
   silentArgs	= '/S'
