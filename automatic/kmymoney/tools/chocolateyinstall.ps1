1ae0a73721bbf3c75e63d3e7e45624195208945b
Chocolatey <chocolatey@realdimensions.net>
2022-12-09 08:25:01 +0000

    AU: 2 updated - kmymoney magicsplat-tcl-tk [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/9fbaf47f737b50c1309fec903bc77a6a1de1f369

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1611-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1612-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '15dda6ead106b5bdaf8bc212f6bc7151cec656f356e1191f7a0bc394fcb41310'
+  checksum64     = 'c96a4f4f1e36d7f60452240b9aa69171000df2e3c1d6ff5f20cb70d08dd56b20'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
