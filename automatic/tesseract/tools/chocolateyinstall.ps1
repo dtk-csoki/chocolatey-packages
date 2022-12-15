30769a0a090be6c2ef73c2e9cc081409525c8ffd
Chocolatey <chocolatey@realdimensions.net>
2022-12-15 00:24:57 +0000

    AU: 2 updated - tesseract webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5f02dd5c6c5d937b00f8d63eae8b5f35bc9faf34

--- automatic/tesseract/tools/chocolateyinstall.ps1
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.2.0.20220712.exe"
-  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.2.0.20220712.exe"
+  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.3.0.20221214.exe"
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.3.0.20221214.exe"
   silentArgs  = '/S'
 }
 
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = 'a23204f3d78a61d9f0d691cf2861454f8e81d042cc94eb537ab01ce6c268637377aaba0d9c946eb1351ecbe52e0edb52c019991b4fa36d350955d38c29ea3273'
+  checksum     = '1a6f56ce6fdc7f28ba11d6d53f4fbe868ccdecce9f7444c3472ec48ad70b0a011bf1f27996a142e29a4392ca1d6b67033ff16415e9705c314d9799c398592038'
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
