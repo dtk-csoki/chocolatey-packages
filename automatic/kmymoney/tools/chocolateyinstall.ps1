276554eeb02f113e2db8f646049367b6fbd1d13f
Chocolatey <chocolatey@realdimensions.net>
2022-12-17 08:24:51 +0000

    AU: 4 updated - chirp chirp.install chirp.portable kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7a33f1b09d6a419d53a16fc9ad33a38933d8b27e

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1618-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1620-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '1119273c3829d0b9ae7974829f0b9836c46a252b4e0bd336d1268dfe526ecfa6'
+  checksum64     = 'a1557bb43ef9da4af3ac1cfbc8c27785f11d6e529ba082876239553962679093'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
