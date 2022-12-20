1e250b5e2b40c79275aa969422a611919fb67933
Chocolatey <chocolatey@realdimensions.net>
2022-12-20 16:25:32 +0000

    AU: 4 updated - automouseclick pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2847c6fd1c8b7db1950b8c9aeb22ab37a938e3d6

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '978A474860B1C4BFBF538CC3E0360613E8C9EA05484026C324A4632C32992D67'
+  checksum     = '919D4D03AB487F41C7637C8FD634299BFE93D48BEF933EA54EA1ECBCCDE9AE3D'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
