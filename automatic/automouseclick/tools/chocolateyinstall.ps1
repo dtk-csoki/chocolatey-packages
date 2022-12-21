c86e27739d1ec514ecedc0d32a42154f58afb9e7
Chocolatey <chocolatey@realdimensions.net>
2022-12-21 00:25:09 +0000

    AU: 2 updated - automouseclick kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/58364deb66e8bc8d75847068337750b94051ed3c

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '919D4D03AB487F41C7637C8FD634299BFE93D48BEF933EA54EA1ECBCCDE9AE3D'
+  checksum     = 'D212A72FEA40EC07158022FD31FE7F89A086B3255F134543DCE009862AF28EE8'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1622-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1623-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'a408b1fa8afdb430e5366fd594d476fdfa1a143983aa302d2931d7b3b66bd779'
+  checksum64     = 'be821cd2fa5a813e0276941b510f85d33c0712dab20a506296fe7381fa7198e0'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
