3e4da6583b52a9d57ae6984935d7cf21ee03783f
chtof <chtof62@gmail.com>
2022-12-25 13:31:26 +0100

    Update update.ps1

--- automatic/littlenavmap/update.ps1
+++ automatic/littlenavmap/update.ps1
@@ -6,7 +6,8 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'albar965/littlenavmap'
-        regex32    = '(/LittleNavmap-win-(?<Version>[\d\.]+(\.beta)?).zip)'        
+        regex32    = '(/LittleNavmap-win32-(?<Version>[\d\.]+(\.beta)?).zip)'
+        regex64    = '(/LittleNavmap-win64-(?<Version>[\d\.]+(\.beta)?).zip)'        
    }
 }
 
@@ -14,14 +15,15 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
