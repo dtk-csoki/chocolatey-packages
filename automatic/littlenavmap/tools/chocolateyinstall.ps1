3c20ad338b796914d1052bd2f0d6466aeec70605
chtof <chtof62@gmail.com>
2022-12-25 14:12:30 +0100

    Update chocolateyinstall.ps1

--- automatic/littlenavmap/tools/chocolateyinstall.ps1
+++ automatic/littlenavmap/tools/chocolateyinstall.ps1
@@ -5,19 +5,15 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
   
-  url32         = 'https://github.com/albar965/littlenavmap/releases/download/v2.8.7/LittleNavmap-win64-2.8.7.zip'
-  checksum32    = '808ff584426d52cc775ad6406da00622f454be95bd4c8fbca42eef4b7235ad5c'
+  url32         = 'https://github.com//albar965/littlenavmap/releases/download/v2.8.7/LittleNavmap-win32-2.8.7.zip'
+  checksum32    = ''
+  url64         = 'https://github.com//albar965/littlenavmap/releases/download/v2.8.7/LittleNavmap-win64-2.8.7.zip'
+  checksum64    = ''
   checksumType  = 'sha256'
-
-  file64      = "$toolsDir\LittleNavmap-win-2.8.0.beta.zip"
 }
 
-If (Get-OSArchitectureWidth -Compare 64) {
-  Get-ChocolateyUnzip @packageArgs
-} Else {
-  Install-ChocolateyZipPackage @packageArgs
-}
-Remove-Item -Path $packageArgs.file64
+Install-ChocolateyZipPackage @packageArgs
+#Remove-Item -Path $packageArgs.file64, $packageArgs.file64
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
