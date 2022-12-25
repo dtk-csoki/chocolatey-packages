f3319a443b9e1a3fe712e4f628e18cd52c5b865e
chtof <chtof62@gmail.com>
2022-12-25 14:13:16 +0100

    Update update.ps1

--- automatic/littlenavmap/update.ps1
+++ automatic/littlenavmap/update.ps1
@@ -1,8 +1,6 @@
 ﻿import-module au
 . ..\..\helpers\GitHub_Helper.ps1
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'albar965/littlenavmap'
@@ -13,18 +11,13 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"            
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
-        }
-
-        "tools\chocolateyinstall.ps1" = @{        
+       "tools\chocolateyinstall.ps1" = @{        
           "(^(\s)*url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
           "(^(\s)*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+          "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-update -ChecksumFor 64
+update
