3b34142b8e9068dac4e56fa459e372bfe2230326
chtof <chtof62@gmail.com>
2022-12-25 13:56:20 +0100

    Update update.ps1

--- automatic/littlenavmap/update.ps1
+++ automatic/littlenavmap/update.ps1
@@ -13,19 +13,18 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+       "legal\VERIFICATION.txt"  = @{            
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+          "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+          "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 64
