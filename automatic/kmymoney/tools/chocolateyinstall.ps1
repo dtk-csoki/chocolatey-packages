915f033a1765847bbc054463107db88d70733fad
Chocolatey <chocolatey@realdimensions.net>
2023-01-01 08:24:53 +0000

    AU: 2 updated - fontforge kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e5e36c3ef5563fe848ef9d384bd6043cd56ddc3c

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1634-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1635-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '9d1622d35cb3601b0fd50182efa7d25a6e407cd091ae2c8753649a3fd93b7b5f'
+  checksum64     = 'd6d0e99a7b976fad2158ed1d2c3e85c09d64b992ad8848e47528e82bcd7e8886'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
