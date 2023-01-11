a14e61ed600516f0d5d1402ce6f3be85b43f007f
Chocolatey <chocolatey@realdimensions.net>
2023-01-11 08:25:05 +0000

    AU: 2 updated - kmymoney sidebar-diagnostics [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/252e7d716c8ee072d815295b3f3f0ee665014b01

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1644-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1645-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '443acb1c49a876a859803f534e579ac82fb09a3540bc8c3f6edbdb058ca80e6a'
+  checksum64     = '0d04aa0c7355d1580394e410f4bda7d7b1e9d93961f41c703e9af51f12719bac'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
