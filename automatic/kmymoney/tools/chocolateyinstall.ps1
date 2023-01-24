23c55a2a51e7ac74c30bf25ebf46c411c453cca3
Chocolatey <chocolatey@realdimensions.net>
2023-01-24 08:25:02 +0000

    AU: 2 updated - ausweisapp2 kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/28ba953377db9898df45104c6a15b24438944b4f

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1657-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1658-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'a47a5820fd8c4b04d87afccf735a30a1edf3a7bff8f623505543bc97a650b961'
+  checksum64     = 'd5773732efdcf14d3df0ec78d8af2315168f59255fa141e5605ae0c38e88b412'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
