0222d875cddc74463d7f99b00bebfa5dce0c6743
Chocolatey <chocolatey@realdimensions.net>
2023-01-25 16:24:58 +0000

    AU: 1 updated - slm [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/75e83bf4f4a8abe2dee47c53026587180400cc15

--- automatic/slm/tools/chocolateyinstall.ps1
+++ automatic/slm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://download.softros.com/SoftrosLANMessengerSetup.msi'
-  checksum     = 'aa71e1c623d3aa185ec02d336bc78ec7041a9547e290083c15d5217e940f87cc'
+  checksum     = '953715cab51ff9060d38da3d21243d62fdef8baaefcecce938d31c6b0817145a'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
