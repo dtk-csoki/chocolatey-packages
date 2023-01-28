115fdf01b0a68e466acd002f3006099acd0316e9
Chocolatey <chocolatey@realdimensions.net>
2023-01-28 16:25:07 +0000

    AU: 1 updated - offset-explorer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/9d8911d61083d25f7902cc5f3fde1f64340c9229

--- automatic/offset-explorer/tools/chocolateyinstall.ps1
+++ automatic/offset-explorer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://www.kafkatool.com/download2/offsetexplorer.exe'
-  checksum      = 'b32ee487c534ba3188bc78f37ea9a0428f5fde399d5acc6b6682372a97231c1a'
+  checksum      = 'e9e6ee3821196782833991faf5d1d4bbf5d2787687ff352067cb2b63736295fb'
   checksumType  = 'sha256'
 
   url64bit      = 'https://www.kafkatool.com/download2/offsetexplorer_64bit.exe'
-  checksum64    = 'f51e3445032ecf2aa7703cd115b456dd36d330bc10593c9b208beda8ea3a1f84'
+  checksum64    = 'be3c26796fcc1cfa941c422d277a2c9924606b77fb7adab9aa2894903f4ae2fd'
   checksumType64= 'sha256'
 
   silentArgs    = '-q'
