a878b3fe116d516228a3896d61bc3fe6c83a3a69
Chocolatey <chocolatey@realdimensions.net>
2023-02-05 16:25:13 +0000

    AU: 3 updated - klayout kmymoney lossless-cut [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f7adc272de2332787d32ba1545c841f8a7cd2f73

--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.28.4-win32-install.exe'
-  checksum      = 'd00ab29540b5ad6a0995e1d53d02bd5c057773ce088126d22c23997d0bf49ef1'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.28.5-win32-install.exe'
+  checksum      = '38fc42ead6209c1c9e0dfeac1eaac47a92e4251f3e82a2d46ff97823aa3a101e'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.28.4-win64-install.exe'
-  checksum64    = 'f87bc8d6d606479e33207cb89a29ebe62be0205f5c8eaeec5db204f48b2d7d00'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.28.5-win64-install.exe'
+  checksum64    = 'bd922d75202de89dd1ca264911600bbae6448b45dec831bb9609c171fe2d6619'
 
   checksumType64= 'sha256'
 
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1670-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1671-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '7a9ce6850300dd9fd21a438a0d97d2d2fd9268a81c9bab76c7a1d228d4bbd934'
+  checksum64     = '62e4472d08aadea51861b47bca6af6dd0c7eeceae92e47822a33154d291c7062'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
