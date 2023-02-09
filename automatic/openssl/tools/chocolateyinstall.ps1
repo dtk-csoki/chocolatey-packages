1c94efd6e527d6de21e73bb38ca3e67349af0e84
Chocolatey <chocolatey@realdimensions.net>
2023-02-09 00:25:16 +0000

    AU: 1 updated - openssl [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/4b88378746af58547fb3d03b32e5ed9e06057d0a

--- automatic/openssl/tools/chocolateyinstall.ps1
+++ automatic/openssl/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\Win32OpenSSL-1_1_1s.exe"
-  file64      = "$toolsDir\Win64OpenSSL-1_1_1s.exe"
+  file        = "$toolsDir\Win32OpenSSL-1_1_1t.exe"
+  file64      = "$toolsDir\Win64OpenSSL-1_1_1t.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
