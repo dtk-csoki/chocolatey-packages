a00cff59d302f48dd1bc6837f3635bf912e743f2
Chocolatey <chocolatey@realdimensions.net>
2023-02-17 16:25:26 +0000

    AU: 2 updated - automouseclick lossless-cut [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fc36f623a3f0a489eac5c0d7c47e7085606c33a1

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'B9473B7B566544A7F332581734D60828AF6D592A6B3CD863696DD099D059C401'
+  checksum     = 'D5499EE8BF7E38D53AF5B45128847A1FD87D09B8E9A3CF4E96C944DAA9AA45E3'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
