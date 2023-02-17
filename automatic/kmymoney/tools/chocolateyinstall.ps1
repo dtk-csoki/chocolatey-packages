6dc3c2ae1ead2add9e2b4e69cee5c12bffec00d2
Chocolatey <chocolatey@realdimensions.net>
2023-02-17 08:25:06 +0000

    AU: 2 updated - kmymoney lossless-cut [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ccdcc24a91f22d759875ab86b871404f7ad3c1dd

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1682-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1683-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '9913214b64ccd6e5c6b55b3a573d5afb13026b65db6d8c61efa8164e2a103647'
+  checksum64     = '1721788eefdd1c3616128008fa9f791f0b70f09fa00e811c1e051971487cd9ec'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
