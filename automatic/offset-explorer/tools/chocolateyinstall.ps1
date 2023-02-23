302f6a52f119e1ae4ac4fd4cb88228d911941584
Chocolatey <chocolatey@realdimensions.net>
2023-02-23 16:24:58 +0000

    AU: 3 updated - offset-explorer spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/41e7b0ada0b2591bf5bf61de30f887961f004140

--- automatic/offset-explorer/tools/chocolateyinstall.ps1
+++ automatic/offset-explorer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://www.kafkatool.com/download2/offsetexplorer.exe'
-  checksum      = 'e9e6ee3821196782833991faf5d1d4bbf5d2787687ff352067cb2b63736295fb'
+  checksum      = 'aeebb5fd1622da4fa17590705ab9abb7be5233653c2b1f39b6b1d4362e755a44'
   checksumType  = 'sha256'
 
   url64bit      = 'https://www.kafkatool.com/download2/offsetexplorer_64bit.exe'
-  checksum64    = 'be3c26796fcc1cfa941c422d277a2c9924606b77fb7adab9aa2894903f4ae2fd'
+  checksum64    = '5eccc7eeee45d279502075330c8265713d8deeccffe6e324f117642e20d12dd4'
   checksumType64= 'sha256'
 
   silentArgs    = '-q'
