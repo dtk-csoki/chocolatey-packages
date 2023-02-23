1b0c5a365e462ff072c9019b76c743d2829380db
Chocolatey <chocolatey@realdimensions.net>
2023-02-23 08:25:32 +0000

    AU: 6 updated - dontsleep dontsleep.install dontsleep.portable jami kmymoney multipar.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/0dd31eec410861da7e25ceb0db0472acc4099d1f

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1688-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1689-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'dd1e26e2fe3f408e1a64857eb4b916e9087e79aab6abc4b879ef0cab876e21b4'
+  checksum64     = 'dc33d5ef5a2c44be2a5f433b3fa71e2db5b717a0c97fce83188f0b22941de609'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
