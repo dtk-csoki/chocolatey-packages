8885123a1df59c1d86e2b1612670cb02451b6185
Chocolatey <chocolatey@realdimensions.net>
2023-02-25 07:48:33 +0000

    AU: 2 updated - kmymoney plantronicshub [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/064d3f9080970cf0fff5a3b183c2d8adb659b001

--- automatic/plantronicshub/tools/chocolateyinstall.ps1
+++ automatic/plantronicshub/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
-  checksum     = '62d7296adf04691eb55611dead1f5ef1d8037b685718721848169fc9b69fb841'
+  checksum     = 'f7a0c184eca4bc9bbc025bbc9addf81801ae0dc6c6a36bb7a3f1942c21eefe04'
   checksumType = 'sha256'
 
   silentArgs   = '/install /quiet /norestart'
