e0cbb4ed1c3d99234b736d6c7297ef0d717b6c23
chtof <chtof62@gmail.com>
2023-02-25 10:12:21 +0100

    Update chocolateyInstall.ps1

--- automatic/vuescan/tools/chocolateyInstall.ps1
+++ automatic/vuescan/tools/chocolateyInstall.ps1
@@ -5,17 +5,17 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url            = 'https://www.hamrick.com/files/vuex3297.exe'
-  checksum       = '9d3043dbc02082330a6c571a1affced8a6958f4812f0eae40a12f5c29985e05c'
+  checksum       = '8a2532096651c8f4c7602880f28872c6ada0bd5faf8ed130122da6290cc0015b'
   checksumType   = 'sha256'
 
   url64          = 'https://www.hamrick.com/files/vuex6497.exe'
-  checksum64     = 'a56a41326092ae97a0fc74430d0595c58596a7dc858fe3e9fb840dbd330c3760'
+  checksum64     = 'ba10379158f53c78bff5689d32e70379805c30fdadb6d8b5bc7f8298d60697ca'
   checksumType64 = 'sha256'
 }
 
-if (Get-ChildItem Cert:\CurrentUser\TrustedPublisher\|Where subject -eq "CN=Hamrick Software, O=Hamrick Software, L=Sunny Isles Beach, S=Florida, C=US") {
+If (Get-ChildItem Cert:\CurrentUser\TrustedPublisher\|Where subject -eq "CN=Hamrick Software, O=Hamrick Software, L=Sunny Isles Beach, S=Florida, C=US") {
   Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
-} else {
+} Else {
   Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyInstallCertificate.ahk`""
 }
 Install-ChocolateyPackage @packageArgs
