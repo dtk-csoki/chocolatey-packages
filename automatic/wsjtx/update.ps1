ca82cbc3874be0ccf1e30eb326e940cdf159f495
chtof <chtof62@gmail.com>
2023-02-25 10:41:20 +0100

    Update update.ps1

--- automatic/wsjtx/update.ps1
+++ automatic/wsjtx/update.ps1
@@ -3,32 +3,40 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases = 'https://physics.princeton.edu/pulsar/k1jt/wsjtx.html'
-    $regex    = 'wsjtx-(?<Version>[\d\.]+)-win32.exe$'
+    $releases = 'https://wsjt.sourceforge.io/wsjtx.html'
+    $regex32  = 'wsjtx-[\d\.]+-win32.exe$'
+    $regex64  = 'wsjtx-(?<Version>[\d\.]+)-win64.exe$'
 
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+    $url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex32).href
+    $url64 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex64).href
 	
     return @{
         Version = $matches.Version
-        URL32   = 'https://physics.princeton.edu/pulsar/k1jt/' + $url.href }
+        URL32   = Get-RedirectedUrl $url32
+        URL64   = Get-RedirectedUrl $url64
+    }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"  
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"  
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+        }
+
+        "wsjtx.nuspec" = @{
+            "(<docsUrl>https://wsjt.sourceforge.io/wsjtx-doc/wsjtx-main-)2.6.1(.html</docsUrl>)" = "`${1}$($Latest.Version)`${2}"
+            "(<releaseNotes>https://wsjt.sourceforge.io/wsjtx-doc/Release_Notes_)2.6.1(.txt</releaseNotes>)" = "`${1}$($Latest.Version)`${2}"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none
