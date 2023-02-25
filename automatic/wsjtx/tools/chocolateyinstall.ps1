b67ea436380b0dd0fa42b47dc02f5a9afb694b2a
chtof <chtof62@gmail.com>
2023-02-25 10:41:17 +0100

    Update chocolateyinstall.ps1

--- automatic/wsjtx/tools/chocolateyinstall.ps1
+++ automatic/wsjtx/tools/chocolateyinstall.ps1
@@ -3,7 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  file         = "$toolsDir\wsjtx-2.5.4-win32.exe"  
+  file         = "$toolsDir\wsjtx-2.6.1-win32.exe"
+  file64       = "$toolsDir\wsjtx-2.6.1-win64.exe"
   silentArgs   = '/S'
 }
 
