a709efcff72d1c34b173d92254af21b4fed3a990
chtof <chtof62@gmail.com>
2023-02-26 09:55:20 +0100

    Update chocolateyinstall.ps1

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
 
   url             = 'https://www.keepersecurity.com/desktop_electron/Win32/KeeperSetup32.msi'
-  checksum        = '21b829a2eeb9ad135b073e0b4242e48cd4f07b8ce24ba1b955ad27bc16408876'
+  checksum        = '663B03D923D13360449E3400D8AB4E7203EA9B960F3AD9B4C60648B442668B2F'
   checksumType    = 'sha256'
 
   url64           = 'https://www.keepersecurity.com/desktop_electron/packages/KeeperPasswordManager.msixbundle'
-  checksum64      = '7b31bb10f0ec1d39946411e812929afbff30eebb6aba62bf5661365551040111'
+  checksum64      = 'E07937D67F6A849DA75139CF63CA01DBD7F0AB86807346776D60377EDA4BD0C0'
   checksumType64  = 'sha256'
 
   silentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
@@ -22,6 +22,23 @@ If (Get-OSArchitectureWidth -eq 64){
   If ($IsCorrectBuild -lt "18362") {
     Throw "This package requires at least Windows 10 version 1903/OS build 18362.x."
   }
+
+  [version]$AppxVer = (Get-AppxPackage -name "$packageArgs.AppxPackageName").Version
+
+  If ($AppxVer -gt [version]$version) {
+    # you can't install an older version of an installed appx package, you'd need to remove it first
+    Write-Warning "The installed $version version is newer than this package version, it may have autoupdated on your current OS..."
+    } ElseIf ($AppxVer -Match [version]$version) {
+      If($env:ChocolateyForce) {
+        # you can't install the same version of an appx package, you need to remove it first
+        Write-Host Removing already installed version first.
+        Get-AppxPackage -Name $packageArgs.AppxPackageName | Remove-AppxPackage
+      } else {
+        Write-Host The $version version of Windows-Terminal is already installed. If you want to reinstall use --force
+        return
+      }
+  }
+
   Add-AppPackage -Path $packageArgs.url64
 } Else {
   # 32 bits
