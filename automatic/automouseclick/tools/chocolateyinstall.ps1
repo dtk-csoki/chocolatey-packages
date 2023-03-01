52278e2ade3a63b57970b6893c93f32d75d3cffc
Chocolatey <chocolatey@realdimensions.net>
2023-03-01 16:24:52 +0000

    AU: 3 updated - automouseclick chirp.install chirp.portable [skip ci]

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '5DDD72FEA59D361C02725C95699677760F18247BA9B58ADB5A0E6996F381B945'
+  checksum     = '287A2547E4D3D7E0DBF3BF0969DF1DD698961A80039A7704592E05DC966AA60A'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20230224-installer.exe"
+  file          = "$toolsDir\chirp-next-20230301-installer.exe"
   silentArgs    = '/S'
 }
 
