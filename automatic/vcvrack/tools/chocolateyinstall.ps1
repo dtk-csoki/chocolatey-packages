40846b395ede2fbd45ccf4d45969aae83412bab6
chtof <chtof62@gmail.com>
2023-03-04 19:36:32 +0100

    Update chocolateyinstall.ps1

--- automatic/vcvrack/tools/chocolateyinstall.ps1
+++ automatic/vcvrack/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\RackFree-2.1.2-win.exe"
+  file64      = "$toolsDir\RackFree-2.2.3-win-x64.exe"
   silentArgs  = '/S'
 }
 
