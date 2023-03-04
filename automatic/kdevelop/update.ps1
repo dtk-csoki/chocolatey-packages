4c685244b38bc86d01a562c217d2e0d54e3953aa
chtof <chtof62@gmail.com>
2023-03-04 09:05:10 +0100

    Update update.ps1

--- automatic/kdevelop/update.ps1
+++ automatic/kdevelop/update.ps1
@@ -1,24 +1,11 @@
-﻿import-module au
-
-$releases = 'https://www.kdevelop.org/download'
-
-function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-     $regex   = 'kdevelop-(?<Version>[\d\.]+)-x64-setup.exe$'
-     $download_page.links | ? href -match $regex | Select -First 1 | Out-Null
-     $version = $matches.Version
-     $versionRelease = $version -Replace "\.", ""
-
-     return @{ Version = $version ; VersionRelease = $matches.versionRelease }
-}
+﻿. $PSScriptRoot\..\kdevelop.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
-            "<releaseNotes>https://www.kdevelop.org/news/kdevelop-(.*?)-released</releaseNotes>" = "<releaseNotes>https://www.kdevelop.org/news/kdevelop-$($Latest.VersionRelease)-released</releaseNotes>"
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update
+update -ChecksumFor none
