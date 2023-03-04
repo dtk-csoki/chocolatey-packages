2e6afc0e9c139e133a84933a3c1df8cada4eb064
chtof <chtof62@gmail.com>
2023-03-04 09:05:40 +0100

    Update update.ps1

--- automatic/kid3/update.ps1
+++ automatic/kid3/update.ps1
@@ -2,41 +2,41 @@ import-module au
 
 function global:au_BeforeUpdate {
   Get-RemoteFiles -NoSuffix -Purge
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
   $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 function global:au_GetLatest {
-	$releases = 'https://kid3.kde.org'
-  $regex64  = 'kid3-(?<Version>[\d\.]+)-win32-x64.zip'
-	# $regex32  = 'kid3-([\d\.]+)-win32.zip'
+  $releases = 'https://kid3.kde.org'
+  $regex  = 'kid3-(?<Version>[\d\.-]+)-win32-x64.zip'
 
-	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  # $download_page.links | ? href -match $regex32 | Out-Null
-  # $file32  = $matches.0
-	$download_page.links | ? href -match $regex64 | Out-Null
-  $file64  = $matches.0
-  $version = $matches.Version
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing  
+  $url = ($download_page.links | ? href -match $regex | Select -First 1).href
+  $file  = $matches.0
+  $version = $matches.Version -replace '-', '.'
   
-	return @{
+  return @{
     Version = $version    
-    # URL32   = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/kid3/kid3/' + $version + '/' + $file32)
-    URL64   = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/kid3/kid3/' + $version + '/' + $file64)
+    URL32   = Get-RedirectedUrl $url
   }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{          
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          "(?i)(^\s*file32\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
           "([$]toolsDir `"kid3-)[\d\.]+(-win32[$]{architectureWidth}\\kid3.exe)`"" = "`${1}$($Latest.Version)`${2}`""          
         }
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -NoCheckUrl
