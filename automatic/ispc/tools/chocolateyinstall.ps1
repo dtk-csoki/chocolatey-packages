b705c15e447f4ef50c7d2f1586889e8013789635
chtof <chtof62@gmail.com>
2023-03-04 19:51:51 +0100

    Update chocolateyinstall.ps1

--- automatic/ispc/tools/chocolateyinstall.ps1
+++ automatic/ispc/tools/chocolateyinstall.ps1
@@ -1,12 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$packageArgs = @{
+<#$packageArgs = @{
   packageName = $env:ChocolateyPackageName
   fileType    = 'MSI'
 
   file        = "$toolsDir\ispc-v1.18.0-windows.msi&response-content-type=application%2Foctet-stream"
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
+}#>
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\ispc-v1.18.0-windows.msi&response-content-type=application%2Foctet-stream"  
 }
 
 Install-ChocolateyPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "InfiniTex.lnk"
+$targetPath = Join-Path $toolsDir "InfiniTex-0.9.16.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
