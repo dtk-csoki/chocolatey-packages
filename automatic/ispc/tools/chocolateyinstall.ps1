daa1470def34aa5a65bf8cf9daea35eab92229f8
chtof <chtof62@gmail.com>
2023-03-04 20:48:03 +0100

    Update chocolateyinstall.ps1

--- automatic/ispc/tools/chocolateyinstall.ps1
+++ automatic/ispc/tools/chocolateyinstall.ps1
@@ -5,20 +5,14 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   packageName = $env:ChocolateyPackageName
   fileType    = 'MSI'
 
-  file        = "$toolsDir\ispc-v1.18.0-windows.msi&response-content-type=application%2Foctet-stream"
+  file        = "$toolsDir\ispc-v1.19.0-windows.zip"
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
 }#>
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\ispc-v1.18.0-windows.msi&response-content-type=application%2Foctet-stream"  
+  file        = "$toolsDir\ispc-v1.19.0-windows.zip"  
 }
 
-Install-ChocolateyPackage @packageArgs
-
-# Install start menu shortcut
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "InfiniTex.lnk"
-$targetPath = Join-Path $toolsDir "InfiniTex-0.9.16.exe"
-Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+Get-ChocolateyUnzip @packageArgs
