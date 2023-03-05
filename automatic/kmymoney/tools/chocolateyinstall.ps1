d34020acca62b7c5182ee083a49d0fa82b4fed2c
Chocolatey <chocolatey@realdimensions.net>
2023-03-05 08:24:56 +0000

    AU: 4 updated - clavier-plus clavier-plus.install clavier-plus.portable kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/c8bee6f30a1bd6a89ceea1ab7c13f9f4e1a8eb68

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1698-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1699-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'f71cd8e4323e4da24a22d896d0429b3f973064019b5e3f7b86e74c07a99d7300'
+  checksum64     = '96e5fe5e0dd7ee906c19caf5192264534a82ca06601242db89e6d268f8fd3c1a'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
