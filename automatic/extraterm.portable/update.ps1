28a071be4d6d35d87bfbacb3d7c5919ded862a4b
chtof <chtof62@gmail.com>
2023-03-06 23:22:32 +0100

    Update update.ps1

--- automatic/extraterm.portable/update.ps1
+++ automatic/extraterm.portable/update.ps1
@@ -6,7 +6,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    $output = github_GetInfo -ArgumentList @{
         repository = 'sedwards2009/extraterm'        
-        regex32    = 'extratermqt-(?<Version>[\d\.]*)-win32-x64.zip$'
+        regex64    = 'extratermqt-(?<Version>[\d\.]*)-windows-x64.zip$'        
    }
 
    # append "-pre" to version
@@ -15,25 +15,20 @@ function global:au_GetLatest {
    return $output
 }
 
-#[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
 function global:au_SearchReplace {
     @{
-        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        "legal\VERIFICATION.txt"  = @{
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
-          "(Join-Path `"[$]toolsDir`" `"extraterm-)[\d\.]+(-win32-x64\\extraterm.exe`")" = "`${1}$($Latest.VersionFile)`${2}"
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`${1}$($Latest.FileName64)`""          
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+If ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor 64
 }
