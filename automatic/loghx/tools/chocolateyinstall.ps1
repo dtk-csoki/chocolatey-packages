2944e5bb4406ce269617f7e5eb971cc4f6050a8c
Chocolatey <chocolatey@realdimensions.net>
2023-03-06 16:25:44 +0000

    AU: 3 updated - kdevelop.install littlenavmap loghx [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e47dc8cad00e665fc386dd3b487e2d333fe88949

--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_719.exe'
-  checksum      = '55fc60769d21e0d71acdac9ba2a6472f67298b759896058c744b55212f070437'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_723.exe'
+  checksum      = '1ddb88a9a1fa2f9a28d30d513088a8084ae8edd1bc370fa07681e6e31e67e715'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
