dcb3caaaa3314708006935bf78442b6b1c4e7503
chtof <chtof62@gmail.com>
2023-03-07 09:19:02 +0100

    Update update.ps1

--- automatic/ffmpeg-batch.install/update.ps1
+++ automatic/ffmpeg-batch.install/update.ps1
@@ -7,16 +7,13 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $releases = 'https://sourceforge.net/projects/ffmpeg-batch/files/'
-    $regex64 = 'FFBatch_AV_Converter_(?<Version>[\d.]+)_64bit.exe'
-    $regex32 = 'FFBatch_AV_Converter_[\d.]+_32bit.exe'
+    $regex64 = 'FFBatch_AV_Converter_(?<Version>[\d.]+)_64bit.exe'    
 
-    $download_page_links = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
-    $url32 = ($download_page_links | ? href -match $regex32 | Select -First 1).href
+    $download_page_links = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links    
 	$url64 = ($download_page_links | ? href -match $regex64 | Select -First 1).href
 
     return @{
-        Version = $matches.Version
-        URL32 = Get-RedirectedUrl ( $url32 )
+        Version = $matches.Version        
         URL64 = Get-RedirectedUrl ( $url64 )
     }
 }
@@ -24,20 +21,17 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`${1}$($Latest.FileName32)`""          
+        "tools\chocolateyinstall.ps1" = @{          
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`${1}$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+If ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
     update -ChecksumFor none
 }
