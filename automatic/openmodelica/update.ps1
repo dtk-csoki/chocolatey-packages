853f2bbb816711aeea2452569dd2e1a6dfcb930a
chtof <chtof62@gmail.com>
2023-03-07 10:43:27 +0100

    Update update.ps1

--- automatic/openmodelica/update.ps1
+++ automatic/openmodelica/update.ps1
@@ -3,23 +3,19 @@ import-module au
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 function global:au_GetLatest {
-  $releases          = 'https://openmodelica.org/download/download-windows'
-  $regexVersionMajor = '/windows/releases/(?<VersionMajor>[\d\.]+).*/$'
-  $regexVersion      = '<a href="https://build.openmodelica.org/omc/builds/windows/.*>(?<Version>[\d\.]+)<br />'
+  $releases = 'https://openmodelica.org/download/download-windows'
+  $regex    = '/windows/releases/(?<Version>[\d\./]+).*$'
+  # $regexVersion      = '<a href="https://build.openmodelica.org/omc/builds/windows/(releases/)?.*>(?<Version>[\d\.]+)(/[\d]+)?<br />'
   # <a href="https://build.openmodelica.org/omc/builds/windows/releases/1.14/final/"><span style="font-size: 13.3333px;">1.14.0<br />
 
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-  $url           = $download_page.links | ? href -match $regexVersionMajor | Select -First 1
-  $versionMajor  = $matches.VersionMajor
-
-  $download_page -match $regexVersion | Select -First 1 | Out-Null
-  $version       = $matches.Version
+  $url           = $download_page.links | ? href -match $regex | Select -First 1
+  $version       = $matches.Version -Replace '/', '.'
 
 	return @{
     Version = $version
-    URL32   = $url.href + '32bit/OpenModelica-v' + $version + '-32bit.exe'
-    URL64   = $url.href + '64bit/OpenModelica-v' + $version + '-64bit.exe'
+    URL32   = $url.href + '/32bit/OpenModelica-v' + $version + '-32bit.exe'
+    URL64   = $url.href + '/64bit/OpenModelica-v' + $version + '-64bit.exe'
   }
 }
 
