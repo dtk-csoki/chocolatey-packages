776ac28fdbe9f4cf84054fb1071f682343828f95
chtof <chtof62@gmail.com>
2023-03-07 16:51:15 +0100

    Create _update.ps1

---
+++ automatic/sonarlint-vs2017/_update.ps1
@@ -0,0 +1,29 @@
+import-module au
+. ..\..\helpers\GitHub_Helper.ps1
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'SonarSource/sonarlint-visualstudio'
+        regex32    = 'SonarLint.VSIX-(?<Version>[\d\.]+)-2017.vsix$'        
+   }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "([$]toolsDir\\SonarLint.VSIX-)[\d\.]+(-2017.vsix)" = "`${1}$($Latest.Version)`${2}"
+        }
+    }
+}
+
+update -ChecksumFor none
