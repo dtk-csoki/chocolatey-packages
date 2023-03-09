24382dd35e25224267ee997f0b42a6ef5a22c757
chtof <chtof62@gmail.com>
2023-03-09 10:50:18 +0100

    Update chocolateyinstall.ps1

--- automatic/kid3/tools/chocolateyinstall.ps1
+++ automatic/kid3/tools/chocolateyinstall.ps1
@@ -4,20 +4,14 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"  
-  file64        = "$toolsDir\kid3-3.9.3-win32-x64.zip"  
-}
-
-if (Get-OSArchitectureWidth -compare 32) {
-    $architectureWidth = ''
-} else {
-    $architectureWidth = '-x64'
+  file64        = "$toolsDir\kid3-3.9.3-1-win32-x64.zip"  
 }
 
 Get-ChocolateyUnzip @packageArgs
-Remove-Item $packageArgs.file64
+Remove-Item $packageArgs.file64 -ErrorAction SilentlyContinue
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Kid3.lnk"
-$targetPath = Join-Path $toolsDir "kid3-3.9.3-win32${architectureWidth}\kid3.exe"
+$targetPath = Join-Path $toolsDir "kid3-3.9.3.1-win32-x64\kid3.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
