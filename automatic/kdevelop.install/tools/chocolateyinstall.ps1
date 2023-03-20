3e48e8538be1c1bf24092f4cd660f213adaa50d4
Chocolatey <chocolatey@realdimensions.net>
2023-03-20 16:26:23 +0000

    AU: 8 updated - geogebra6 geogebra-geometry kdevelop kdevelop.install log4om log4om.install log4om.portable roomarranger [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/4105a2c9db61e504aab47d5874372f5ea0b87231

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://binary-factory.kde.org/job/KDevelop_Release_win64/lastSuccessfulBuild/artifact/kdevelop-5.6.2-1869-windows-cl-msvc2019-x86_64.exe'
-  checksum64     = 'e58c23890318060895dc229e85b194602af0650d1db32da0416057fbda836db9'
+  url64bit       = 'https://binary-factory.kde.org/job/KDevelop_Release_win64/lastSuccessfulBuild/artifact/kdevelop-5.6.2-1870-windows-cl-msvc2019-x86_64.exe'
+  checksum64     = 'fe46fd5f294bf96f50882987ac492963cef420ff459244e800f0b296f605bc85'
   checksumType64 = 'sha256'
 }
 
