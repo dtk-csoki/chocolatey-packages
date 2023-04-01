7af573524ab4ee3a8b0c81373a4f95ee7c3ecc28
Chocolatey <chocolatey@realdimensions.net>
2023-04-01 08:24:39 +0000

    AU: 2 updated - kmymoney moneydance [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/08cd6d129e9cdafdcd2f4ffe1f8c0fe005167cbf

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1725-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1726-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '519d26ff684e1546fd6b703e310c7c8da52ab4eaefe6a1728cf23d1b0033fdf0'
+  checksum64     = '668c21728e5653594523ee2b64b504a3eadf80cd957b46e0fbdfbd3e698c1865'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/moneydance/tools/chocolateyinstall.ps1
+++ automatic/moneydance/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
-  checksum64     = '566c2cdc25532032d40fc5704bfcb85105d833db82ac0ce57b55417a1f06869c'
+  checksum64     = 'd09df9083c209d6b9c4f5257622e7b598366e424293c9ae783479c2660e6f696'
   checksumType64 = 'sha256'
 
   silentArgs     = "-q"
