e3fc058cc7aff9bdb8b5b27f366ed86b90206614
Chocolatey <chocolatey@realdimensions.net>
2023-04-11 08:25:09 +0000

    AU: 3 updated - geogebra6 geogebra-geometry kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/30dc849fd171c2ddae72aa5a381796d6d3ca75fb

--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGeometry-Windows-Installer-6-0-770-0.exe'
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-772-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1735-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1736-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'd9ed32a6721472926289c426290c66931d720cc5fb3dc5b37f6aceb0b555f1de'
+  checksum64     = '815e1c86b500c683627edb645f0d42d3f8956284a53d26682273cebc69d0286a'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
