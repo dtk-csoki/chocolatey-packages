fcf810da3e306b717144ac18f5f83e86d4b4e307
Chocolatey <chocolatey@realdimensions.net>
2023-04-19 00:26:30 +0000

    AU: 2 updated - openmodelica webex-meetings [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/c923c0cb696c6eb14b61aedd86b743b89528b739

--- automatic/openmodelica/tools/chocolateyinstall.ps1
+++ automatic/openmodelica/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.20/0/32bit/OpenModelica-v1.20.0-32bit.exe'
-  checksum      = '5874c035e95e73eb1ff935c63d9c0a7b57126fded8220c4fc22aed2892cb10c8'
+  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.21/0/32bit/OpenModelica-v1.21.0-32bit.exe'
+  checksum      = '1e5517ea062e97f51a3acc70e0c20992f54cddef402778aaa222fccc23888ecd'
   checksumType  = 'sha256'
   
-  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.20/0/64bit/OpenModelica-v1.20.0-64bit.exe'
-  checksum64    = '3e8fd7f4bd7f0f6edebfa504b89acfc5caffdcceecba20b92d1d43de845d5d12'
+  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.21/0/64bit/OpenModelica-v1.21.0-64bit.exe'
+  checksum64    = 'c36a85a4def98910635079ff27a34b776750fb72809d2dd5fea54c30789f1f82'
   checksumType64= 'sha256'
   
   silentArgs	= '/S'
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '5c3e3d25e2960f251b842f63162767b2ac57a9eff6f8eabc6f70914145ee5e74f7a9349fc92344774cee4f12e9f383d3cd8d9a342c939c61ad5bab14672b9163'
+  checksum     = '67257b5192e5f2a5c2c48897198326809cf6f93037c36e9ea4731e96dd336f8435d15e8b0991ccaba8e9187c99939e7a4031fb3d88f8239bf81fce96c55931d4'
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
