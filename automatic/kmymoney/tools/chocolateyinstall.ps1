479c8fe288712614c7fd694642499ad0fb0b3394
Chocolatey <chocolatey@realdimensions.net>
2023-04-27 08:24:50 +0000

    AU: 2 updated - kmymoney virtualhere-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/8ee4ec82e9b39f00aef186988994b00de69a3efe

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1749-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1750-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'd533823ffa4c4c5e733c51791dc3c86b55ae26214d764849e58501c1b14fe22e'
+  checksum64     = '28aa4c071ea491234f5ad08a1092ceb0caa9c0892993489cc69c07e21c5253d0'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
-  checksum64     = 'eff835cab4dd2672ace9f6fa70a82972b586d7f3d5fc8094d53fc9e5b63ba0ae'
+  checksum64     = 'b4b98da4a6d43922b59b982f2c26eaba9fe911be905dd87d1c74564928c5a9ae'
   checksumType64 = 'sha256'
 }
 
