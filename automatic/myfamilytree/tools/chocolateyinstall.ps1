7e3d1721125b10acd5e3ec8a11f1513c8ed2210c
Chocolatey <chocolatey@realdimensions.net>
2023-05-09 00:25:12 +0000

    AU: 4 updated - geogebra6 geogebra-geometry myfamilytree myfamilytree-languagepack [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e248ddd6c9e649901c9ab58b936bf3609007e229

--- automatic/myfamilytree/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   filetype      = 'msi'
 
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.5.9.0.msi'
-  checksum      = 'aca7aec01282510c9269c28dad1959ef3c3c0ae1325716a3139079f45d84aa0f'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.6.0.0.msi'
+  checksum      = '6ee84fbbb46ac853d5e0c65c88a628e1eb8f3e931834a39ecad44b44ca9772d0'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.5.9.0x64.msi'
-  checksum64    = '6c82327e04bb686789a7de327600dc2c94a50667a9fe33c68dc8ba4b44c3d779'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree12.6.0.0x64.msi'
+  checksum64    = '8952b0e9e9b78b56afa14b520f1a08992b937aaa835d0f57f7da7de378cff022'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
