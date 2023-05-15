5c4e86f1dda3660c8e2ed48a275a385057f314cf
Chocolatey <chocolatey@realdimensions.net>
2023-05-15 16:25:03 +0000

    AU: 6 updated - chirp chirp.install chirp.portable flrig geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/aaf02ba7145c2221ee7d6e9aeebc82409ac0f9b6

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20230514-installer.exe"
+  file          = "$toolsDir\chirp-next-20230515-installer.exe"
   silentArgs    = '/S'
 }
 
