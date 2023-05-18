c38c07e6d207afdc82068c57b60090f5b20c095c
chtof <chtof62@gmail.com>
2023-05-18 10:41:22 +0200

    [update] tesseract

--- automatic/tesseract/tools/chocolateyinstall.ps1
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -2,9 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.3.0.20221214.exe"
-  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.3.0.20221214.exe"
+  packageName = $env:ChocolateyPackageName  
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-5.3.1.20230401.exe"
   silentArgs  = '/S'
 }
 
--- automatic/tesseract/update.ps1
+++ automatic/tesseract/update.ps1
@@ -1,45 +1,35 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases = 'https://github.com/UB-Mannheim/tesseract/wiki'
-    $regex32  = 'tesseract-ocr-w32-setup-v([\d\.-]+(-alpha|-rc)?([\d\.]+)?).exe'
-    $regex64  = 'tesseract-ocr-w64-setup-v(?<Version>[\d\.]+(-alpha|-rc)?([\d\.]+)?).exe'    
+    $releases = 'https://github.com/UB-Mannheim/tesseract/wiki'    
+    $regex64  = 'tesseract-ocr-w64-setup-v?(?<Version>[\d\.]+(-alpha|-rc)?([\d\.]+)?).exe'    
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url32         = $download_page.links | ? href -match $regex32
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
     $url64         = $download_page.links | ? href -match $regex64
     $version       = $matches.Version -Replace "([\d\.]+)(-alpha)([\d\.]+)", '$1$3$2'
     $version       = $matches.Version -Replace "([\d\.]+)(-rc)([\d\.]+)", '$1$3'
 
     return @{
-        Version = $version
-        URL32   = $url32.href
+        Version = $version        
         URL64   = $url64.href
     }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+       "legal\VERIFICATION.txt"  = @{                        
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        "tools\chocolateyinstall.ps1" = @{          
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor None
