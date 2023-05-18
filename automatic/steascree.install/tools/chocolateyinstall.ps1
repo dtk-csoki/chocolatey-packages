d9938e463b15923274bb1cc36f6c3bf40fc13377
chtof <chtof62@gmail.com>
2023-05-18 11:37:04 +0200

    [streascree] add option to create desktop shortcut

--- automatic/steascree.install/tools/chocolateyinstall.ps1
+++ automatic/steascree.install/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp = Get-PackageParameters
+$DesktopShortcutPath = "$env:PUBLIC\Desktop\SteaScree.lnk"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
@@ -8,3 +10,23 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
+function InstallShortcut {
+  param (
+    $ShortcutPath
+  )
+
+  $installLocation = "${env:ProgramFiles(x86)}\SteaScree"
+
+  $shortcutArgs = @{
+    shortcutFilePath = $ShortcutPath
+    workingDirectory = "$installLocation"
+    targetPath       = "$installLocation\SteaScree.exe"
+  }
+
+  Install-ChocolateyShortcut @shortcutArgs
+}
+
+If ($pp['DesktopShortcut']) {
+    InstallShortcut $DesktopShortcutPath
+}
