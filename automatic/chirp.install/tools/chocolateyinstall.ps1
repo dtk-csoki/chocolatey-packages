dd4458760e1b33c750463baf9cd8f577953d582b
Chocolatey <chocolatey@realdimensions.net>
2023-06-01 16:25:30 +0000

    AU: 4 updated - chirp chirp.install chirp.portable genymotion [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2620c0ac89edf3a3d2369279f45ec99afee6d5a7

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20230531-installer.exe"
+  file          = "$toolsDir\chirp-next-20230601-installer.exe"
   silentArgs    = '/S'
 }
 
