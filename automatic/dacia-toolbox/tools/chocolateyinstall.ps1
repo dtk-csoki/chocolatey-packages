b78dec7c082df74e6b16543751cfd28d047e5ff1
Chocolatey <chocolatey@realdimensions.net>
2023-06-01 08:25:40 +0000

    AU: 2 updated - dacia-toolbox kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/27b272fb2be18bfbd49bc82d01b5549e757527c8

--- automatic/dacia-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-toolbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://download.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2022_05_13_12_03/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
-  checksum     = 'd048aa614726a5ba486f2d7828296ad2b04540cecbb96696f6e1495eb30c1dc8'
+  url          = 'https://download.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2023_05_23_18_11/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
+  checksum     = '7ec7a347aab2d81a353ab1231f2958a818b5a41d25635bf7f328fa39da478d74'
   checksumType = 'sha256'
 
   silentArgs   = "/S"
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1781-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1785-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '24f155008f3732c6e343aeb2e3f4e742e49e601ad1da96746c2401e927fe372c'
+  checksum64     = '5f10a4f78c0b718609c0c3aad71b2a5068ab64b284f4987390fdaadf8e14073d'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
