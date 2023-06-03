2e20bed53f4a212096fea000871f014caa8a3e3f
Chocolatey <chocolatey@realdimensions.net>
2023-06-03 16:26:31 +0000

    AU: 3 updated - ocenaudio ocenaudio.install ocenaudio.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/6428def546e07dfa225b21ca6727361663bbd270

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
   url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio.exe'
-  checksum       = 'd80b504afb54d3f5771bc9a31cb496db5ed2d07be2f8e8680292fdd80b31de23'
+  checksum       = 'a122f3d1896c6b84b53f00f440bb991abb3f333bd2b6e35cc2b0df3501334738'
   checksumType   = 'sha256'
 
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64.exe'
-  checksum64     = '295e51fbaafd05519c81781cf00ff8646ffd6b07be9a328107241ab9c706a266'
+  checksum64     = '18a3cbdae8a1ffd3c066a1974d57db2a95265ce7d8ddaac69d31d721da7ca4c6'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
--- automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   unzipLocation  = "$toolsDir"
 
   url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_portable.zip'
-  checksum       = '8c70b4ce308f8203328bf9e8791baf9182ecf49089a4035a36253f864b4301b5'
+  checksum       = '860aa6fc2f8118097aa3c5bf5e11d4765a65e0d599bc01b155a98a086fe9c969'
   checksumType   = 'sha256'
 
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64_portable.zip'
-  checksum64     = 'd1291f1fcbbafca04581062105046c479af4efe6777f4cb6c755604c4a37657c'
+  checksum64     = '0778b076ce3f3ca5eef2e435cbbe114532d3e8a4c1b0e6f4e43e469e47ec5047'
   checksumType64 = 'sha256'
 }
 
