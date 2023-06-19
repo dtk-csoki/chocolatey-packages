5e66b719ded5d064a9f5bf370b8168f683e43b61
Chocolatey <chocolatey@realdimensions.net>
2023-06-19 08:25:08 +0000

    AU: 2 updated - kmymoney webex-meetings [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b84f9f305ec2e0c7f6a404c00815890b7a29ce80

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1803-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1804-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'b024409898a5607059d8e0cb8f1125e6995f5917d5ad99d84d3f84a063abcc9c'
+  checksum64     = '257ac27a6a18f866e9e558952873afcdeb8b9cd59c30fb1b4010361eb0869463'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '29ca7010b86f619a65ac368a1b14a185dca0120b78946bdb9cb1d9aed8d4982796bdd73307d76083baf5a008e42665cba74e1270616628289523960a18133a09'
+  checksum     = 'b30fe5a3303a0505fb1dde1c83ceb7313b13269c054d2ade2ea655fc38af8bbbb6af1cc6bb70ac7b49095152a6dd1078cf42b5873c05d0352002b8d83e1fc231'
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
