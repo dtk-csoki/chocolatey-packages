39da4c3a73c3e21037f4e911e2b7c5d4c717b235
Chocolatey <chocolatey@realdimensions.net>
2023-06-21 08:26:07 +0000

    AU: 2 updated - bbc-iplayer kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/04f8cdca5989ab5346afd4fa76c5222abae72777

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1805-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1806-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'bf13c43cdd9215a46175bb1557ed5f81082a8410914b968d9e3ba67aca3e8edc'
+  checksum64     = '3e430938cec969d9c0abceb69d6ee28ec699213345e6e4c520821a66c29bf1d0'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
