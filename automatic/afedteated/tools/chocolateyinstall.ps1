638d9a44a679e426f42139e69bd009269ad2adb8
Chocolatey <chocolatey@realdimensions.net>
2023-07-05 08:29:38 +0000

    AU: 2 updated - afedteated kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b1a6ff90509f3d6b4e998c3cc09ce411f72861a7

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '8a33dc6312be41d585a76d0055c5f50a937445547fb4064327749b120fe2099b008b224e2a86477a66d676a5d59b60fb52f4b081ac448e179dc7d1c50d9fd368'
+  checksum     = '544b9fecc1923d12ae027b48383fe4811b3684d7ddd797b2096b10a35fe9a1670d125110f4a2e77222bd6d434681af738cc519be326bfcc551e3fd141a27e663'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1819-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1820-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '1bf093512f1889a81197c9082c77366c0074f9009814acf2721f037d1e40b040'
+  checksum64     = '92b4b2a0c04fa2870f8dea0c2fb234b6da6aefc32fb976d9a3cb272b7903ca96'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
