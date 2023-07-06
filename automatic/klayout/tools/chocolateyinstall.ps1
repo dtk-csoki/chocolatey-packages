273b558b6b60bc954a8179d17b94bee20e4970a1
Chocolatey <chocolatey@realdimensions.net>
2023-07-06 00:25:03 +0000

    AU: 4 updated - carotdav carotdav.install carotdav.portable klayout [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b775b7e5395abbd35e2e03ff72222dc032bc6554

--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.28.9-win32-install.exe'
-  checksum      = '3f029819e53dfe0a3cf803702665b0892163b97038a10406ebf59c138c00b740'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.28.10-win32-install.exe'
+  checksum      = '5c42b61ead2c3fcacfa772636c2fa049efd4863787ce73df5a8e03163f20b1f3'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.28.9-win64-install.exe'
-  checksum64    = 'f4bdf3bca4ca4b0958900900543a989d6ca2aadc699bdf74f3a46ae9517e330c'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.28.10-win64-install.exe'
+  checksum64    = '5d6e7fa2446ad6b0c7d466e69ed038f0ed088b1ea642b27c6ff905a5f3d8b540'
 
   checksumType64= 'sha256'
 
