01e806cdff5711e8b7c56510afc387b8955b81d8
chtof <chtof62@gmail.com>
2023-07-08 01:09:06 +0200

    Update chocolateyinstall.ps1

--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -5,10 +5,6 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
-  url            = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui32.exe'
-  checksum       = 'b426eeabdaee09f2358f3a6e299d96e28e6f4ecd422912083703f20855aa1905'
-  checksumType   = 'sha256'
-
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
   checksum64     = '7e445f20d92627a589fd06a2e761c0279a8d95b2dea3886cdde39042d4d0382e'
   checksumType64 = 'sha256'
