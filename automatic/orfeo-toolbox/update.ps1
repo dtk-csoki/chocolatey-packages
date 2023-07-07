30d1b16d9f392012c23c5900eceb644297365182
chtof <chtof62@gmail.com>
2023-07-08 00:54:51 +0200

    Update update.ps1

--- automatic/orfeo-toolbox/update.ps1
+++ automatic/orfeo-toolbox/update.ps1
@@ -3,13 +3,19 @@ import-module au
 
 function global:au_GetLatest {
      $releases = 'https://www.orfeo-toolbox.org/download/'     
-	 $regex64  = 'OTB-(?<Version>[\d\.]+)-Win64.zip'
+	 $regex64  = 'OTB-(?<Version>[\d\.]+)(-rc)?(?<VersionRC>\d+)?-Win64.zip'
+     #https://www.orfeo-toolbox.org/packages/OTB-8.1.2-rc1-Win64.zip
 
 	 $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
      $url64         = $download_page.links | ? href -match $regex64
+     $version = $matches.Version
+
+     if ($matches.VersionRC) {
+        $version += "." + $matches.VersionRC + "-rc"
+     }
 
      return @{
-        Version = $matches.Version        
+        Version = $version
         URL64   = $url64.href
     }
 }
