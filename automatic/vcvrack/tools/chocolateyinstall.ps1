7c65f22cafd7df5a6538e3d43633ca6d6caf9395
Chocolatey <chocolatey@realdimensions.net>
2023-07-14 11:18:12 +0000

    AU: 1 updated - vcvrack [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/042c883707cc073ca5504fc267f45eee7f3b6dfa

--- automatic/vcvrack/tools/chocolateyinstall.ps1
+++ automatic/vcvrack/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\RackFree-2.2.3-win-x64.exe"
+  file64      = "$toolsDir\RackFree-2.3.0-win-x64.exe"
   silentArgs  = '/S'
 }
 
