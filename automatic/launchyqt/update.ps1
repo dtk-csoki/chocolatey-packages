be507d0a5e10b2b7bede265be4bc4e49c77307c5
chtof <chtof62@gmail.com>
2023-07-14 10:22:10 +0200

    Update update.ps1

--- automatic/launchyqt/update.ps1
+++ automatic/launchyqt/update.ps1
@@ -1,8 +1,16 @@
 ﻿import-module au
+. ..\..\helpers\GitHub_Helper.ps1
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
-function global:au_GetLatest {    
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'samsonwang/LaunchyQt'
+        regex64    = 'Launchy-([\d\.]+)-win-x64.7z$'
+   }
+}
+
+<#function global:au_GetLatest {    
     $releases = 'https://github.com/samsonwang/LaunchyQt/releases/latest'
     $regex    = '/samsonwang/LaunchyQt/tree/v(?<Version>[\d\.]+)'
     
@@ -14,23 +22,23 @@ function global:au_GetLatest {
         URL32 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-x86.7z'
         #URL64 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-amd64.7z'
     }
-}
+}#>
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            #"(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            #"(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            #"(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            #"(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
+            #"(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+            #"(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          #"(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          #"(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
           "(?i)Launchy-[\d\.]+-win"   = "Launchy-$($Latest.Version)-win"
         }
     }
