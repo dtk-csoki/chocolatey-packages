95723139b2c3e285e55648e22234484de3d22abc
chtof <chtof62@gmail.com>
2023-07-16 11:29:47 +0200

    Update chocolateyinstall.ps1

--- automatic/foldercolorizer/tools/chocolateyinstall.ps1
+++ automatic/foldercolorizer/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = "$toolsDir\FolderColorizer2.exe"
-  silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyInstallPackage @packageArgs
