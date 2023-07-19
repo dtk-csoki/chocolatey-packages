6cdfa9497eace2e860f9dd3b3f2648f3afad38b2
Chocolatey <chocolatey@realdimensions.net>
2023-07-19 08:24:43 +0000

    AU: 2 updated - kmymoney phonerlite.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f1ca78d47e55a16f03c9d7f98cc81785f3dd6de5

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1833-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1834-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '6d76e15d71c3e819a5c4290b92d39fc7b1c57faf8a02fb5f163647b323cb3537'
+  checksum64     = '9f466c3179697574a2e3a8908d94c2b6896e1887b43de06cb721c8f0b2627ae4'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
