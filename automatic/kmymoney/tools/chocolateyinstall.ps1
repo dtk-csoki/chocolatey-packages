61c7c1342cafa856cd671c99f48f4e1cb56583ef
Chocolatey <chocolatey@realdimensions.net>
2023-07-25 08:24:52 +0000

    AU: 2 updated - kmymoney virtualhere-client [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d913d6066fdab9e676400e416a01c34ae761b494

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1839-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1840-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '62d855044362fcbc823427b0d4ca6fb124b6f37e56b5b7dd1f9d438336820fdf'
+  checksum64     = '369c3a387d5504b3650204c9a613a78858ac13ba7d06cd200c8d36878807f499'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
-  checksum64     = '36aa4c9f25ec3945b41cd5e34d6cc5d26f3e4c437aee91a61313fe28c21f41f8'
+  checksum64     = 'ef03e48cbc6e1ef2e4d89eb5a44e13bfda12c12254b4c1aaa3866b1600e931e3'
   checksumType64 = 'sha256'
 }
 
