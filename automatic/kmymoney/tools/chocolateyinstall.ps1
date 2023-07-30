7a02279c65df1b2b8cf7458147aa1e37d1e00e09
Chocolatey <chocolatey@realdimensions.net>
2023-07-30 16:24:54 +0000

    AU: 4 updated - apo-ok apo-ok.install apo-ok.portable kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/298708cbe23b9f28f6b136ea0f31880e74781837

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1844-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1845-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '8c5272c6f47150411655f9fca2caf5cf79e2b2d12e1d052e9c36514c42f8a265'
+  checksum64     = '4dcb5ff3fc59dd9e7903f71a0720ba0d0c7992c48e58e8486952b5879d3ab0d7'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
