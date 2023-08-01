9b9c09262d5591dc405f2068dd2aa4b8af3a4ac1
Chocolatey <chocolatey@realdimensions.net>
2023-08-01 00:25:38 +0000

    AU: 2 updated - automouseclick cuda [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/6489d3133125861e60eb16ebb5048f11212e3a27

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'EF8BB3B9A81DA4B7012D3DE622BFC3C01A6DB748EBA58CF4D53953C92892E67D'
+  checksum     = '537D8C4E2532FA3D804BCC1BE0D23EFDD7ED1CEC7D155B669DA8D3F18C7D0453'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/cuda/tools/chocolateyinstall.ps1
+++ automatic/cuda/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_536.25_windows.exe'
-  checksum     = 'db438aca524f593422aa32c63314285e57b54b31c24033553757cfe5bb7174ff'
+  url          = 'https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda_12.2.1_536.67_windows.exe'
+  checksum     = '84b0fb6f92a5fa5d8d39de375832c8e9836be0221b6d424b4cf4fdff5352667a'
   checksumType = 'sha256'
 
   silentArgs   = '/s'
