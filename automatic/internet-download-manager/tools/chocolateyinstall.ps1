3d01549364ad9aef1907871b2d920b7c56bd0065
Chocolatey <chocolatey@realdimensions.net>
2023-08-02 08:25:24 +0000

    AU: 2 updated - internet-download-manager kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/df933798169104a716a35e174f440b8bfc278889

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman641build15.exe'
-  checksum      = 'c70d80fe3c94371fb693572697cd8627452519d1855d23b1c680dc21b50b2059'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman641build17.exe'
+  checksum      = '3a274efb39a47630560384e556b2ecb94ac4ae9363091179884f34fe70478eb9'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1847-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1848-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'a708063063f14fd3628c03e624ae3cca25602df7f2333c62baa5b7f06e97ca52'
+  checksum64     = 'aae46bdc9513070319a9a5b94ca79927f93b669ca2c6d977833180a44d5b0cfd'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
