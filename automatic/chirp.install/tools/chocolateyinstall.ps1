421b1d1746f3680e9e9f585b1658fca4712657ec
Chocolatey <chocolatey@realdimensions.net>
2023-08-18 16:25:09 +0000

    AU: 6 updated - busybox chirp chirp.install chirp.portable spacedesk-client spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/1d7a058c2f1ec7347895a099b94f56f8a8789a28

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20230816-installer.exe"
+  file          = "$toolsDir\chirp-next-20230818-installer.exe"
   silentArgs    = '/S'
 }
 
