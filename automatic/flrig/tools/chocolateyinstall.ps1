5ae8e0d22e47f3168ba77294fe6d6affad3c7e6e
Chocolatey <chocolatey@realdimensions.net>
2023-08-26 00:24:43 +0000

    AU: 1 updated - flrig [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5a091c1197220350bd6eca1667998717a3b963b6

--- automatic/flrig/tools/chocolateyinstall.ps1
+++ automatic/flrig/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ if ($pp['DIR']){
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\flrig-2.0.02_setup.exe"
+  file        = "$toolsDir\flrig-2.0.03_setup.exe"
   silentArgs  = $silentArgs
 }
 
