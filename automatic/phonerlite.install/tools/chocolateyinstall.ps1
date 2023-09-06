5be8183481c96faee975b8a7fd5224874bd7fdf7
Chocolatey <chocolatey@realdimensions.net>
2023-09-06 16:25:17 +0000

    AU: 6 updated - chirp chirp.install chirp.portable phonerlite phonerlite.install phonerlite.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5fd32d1ae5fc417ab5c8810695d153443d4459ff

--- automatic/phonerlite.install/tools/chocolateyinstall.ps1
+++ automatic/phonerlite.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName    
-  file          = "$toolsDir\PhonerLiteSetup_3.20.exe"    
+  file          = "$toolsDir\PhonerLiteSetup_3.21.exe"    
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
