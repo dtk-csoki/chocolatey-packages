1c30b1ac51ccf28d69251b576ee5241c91f7528e
Chocolatey <chocolatey@realdimensions.net>
2023-09-06 08:25:01 +0000

    AU: 2 updated - kmymoney steelseries-engine [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/44c786a59c74830cf25cd14d991b1c087e1e8bac

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1882-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1883-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '3cbe631b35938d93ecdff9889eeb8b8e7e051b54d43e5adc1e57a444674fcc71'
+  checksum64     = '9604efc76aa958095b832398aa1265d33b4f96163c05b52cae759644f5db63ff'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG42.0.0Setup.exe'
-  checksum      = '7C9426D13058AF2D76AC8AA342AADDF7A5BBAA5F3F8EA419410482E0E26322BF'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG46.0.0Setup.exe'
+  checksum      = ''
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
