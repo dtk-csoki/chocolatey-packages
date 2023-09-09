6b4c8224c55023c8988d61fded9be7bedfad2197
Chocolatey <chocolatey@realdimensions.net>
2023-09-09 08:24:57 +0000

    AU: 2 updated - kmymoney srb2 [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2e8613fe468f97eff31f0a512a2646624de6082c

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1885-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1886-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'd330acc6aef30305fd88cff909b355631ccf1027033031f3d4abb0b78eb8e5f4'
+  checksum64     = '0f88a259ff9eda3aa4dbead04388c17aec731b73d313a520fe271417d614ae0f'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
--- automatic/srb2/tools/chocolateyinstall.ps1
+++ automatic/srb2/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://github.com//STJr/SRB2/releases/download/SRB2_release_2.2.12/SRB2-v2212-Installer.exe'
-  checksum      = 'a5f5d84ba123e20aa81e1da8e6783635a5d12994eb79c60c3451271dcf80d3a3'
+  url           = 'https://github.com//STJr/SRB2/releases/download/SRB2_release_2.2.13/SRB2-v2213-Installer.exe'
+  checksum      = '74da34ca3ba1962015714afa8535051b3ca8414ace28b0bdc13a176f6218d703'
   checksumType  = 'sha256'
   
   
