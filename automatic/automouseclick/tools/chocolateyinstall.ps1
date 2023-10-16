37bc8cec15f1ca6ee9636fd129e657827d5b38c4
Chocolatey <chocolatey@realdimensions.net>
2023-10-16 16:25:34 +0000

    AU: 6 updated - automouseclick chirp chirp.install chirp.portable geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f12f128e50cd042bd56ed892e5456931a61cee25

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'D88E2D8C6A8AC3CA2AB84EBBA9E5E0E99F0EA633ADC3D3604E05B3093FF60B61'
+  checksum     = 'DFF92DFD259C68BDC715FDE4A0321D7A31BBCD70F10366FF77AD91CA14D0E436'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/geogebra6/tools/chocolateyinstall.ps1
+++ automatic/geogebra6/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGraphing-Windows-Installer-6-0-805-0.exe'
+$file     = 'GeoGebraGraphing-Windows-Installer-6-0-806-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
