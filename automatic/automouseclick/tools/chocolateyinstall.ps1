a2b91c9ad646529b4d5061727233c4122d9e6a9a
Chocolatey <chocolatey@realdimensions.net>
2023-10-20 16:24:56 +0000

    AU: 3 updated - automouseclick geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/445287d7b7e385e63e7200d58bef0cbfd34793c0

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '665A9CA26AE6FC851DF90A687D4D94E3F230C2667E61DF276758A2DE37E4BCFE'
+  checksum     = 'DA20A1EE3C161E29DE855DA3650293BDAF4741C7D2D81AEFD6FF38EB4E181027'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGeometry-Windows-Installer-6-0-806-0.exe'
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-807-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
