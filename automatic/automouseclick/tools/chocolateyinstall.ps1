1219ea185df401741641e3ce8920d6a39d740cd0
Chocolatey <chocolatey@realdimensions.net>
2023-10-25 00:25:08 +0000

    AU: 2 updated - automouseclick webex-meetings [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/9c52e484ceea3f6f884bc9520d0eed6c2b1aa7aa

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '8A01D07E7A15FAE558F43DBA9B52E5AC31BAB1A1A0072D35F67A01CE6FF7637E'
+  checksum     = '51A4B0755326251B407F446D081E241B9AA775F52DF6A350609950BF2033864B'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '66f04c1a9647bddba34ad078e23fe2d83b50cc9bb827567c3c6e50c1f1b4ee55fafdd378593014589126586bd5d627764a72334958a27c0c50355e87357b6a5a'
+  checksum     = '57d4874aac12a6efeeaeab7ffab894fdacb1712e43e581069ad7369a0be540d8a0b9555a46996fa16eec06143c1dd4de806725a1211c09b1982244d034b592cd'
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
