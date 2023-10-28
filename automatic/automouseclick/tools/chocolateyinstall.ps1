6f8932d28897f9bca0ac41f6c0c5f366e7c29e1b
Chocolatey <chocolatey@realdimensions.net>
2023-10-28 00:25:03 +0000

    AU: 5 updated - automouseclick ffmpeg-batch ffmpeg-batch.install ffmpeg-batch.portable jami [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/bb60daef57b149c53720b1e2acd699a18ca5dd39

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '975BFB159B38B6BEECB5A8AEBF72D8F97F44D2790145DFFD5E4A9E0129F41F60'
+  checksum     = '36F018E6D0F0E8DFCA98335CE9E91A0A2D66C095AE6BD7613F982D8F003C2015'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
