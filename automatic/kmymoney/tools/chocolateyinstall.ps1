f588d8b3866422560ffea76eb5b193cf805be781
Chocolatey <chocolatey@realdimensions.net>
2023-11-02 08:24:39 +0000

    AU: 4 updated - kmymoney moonlight-qt moonlight-qt.install moonlight-qt.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5d5256d05b31e4048328d1de7299884742a6c811

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1939-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1940-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = 'b7cbd532feaf21a92e703575791d5b5ed37eb074a9632908f4ceb6a8cdf1a171'
+  checksum64     = 'f39ca0d0366c152a9e9170d6b0bafec350148ca0e26aa0cd4dc8e02a13ce5ad6'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
