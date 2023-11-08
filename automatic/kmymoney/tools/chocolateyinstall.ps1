01e819230f94409825b6bb9ffc6895943051f29d
Chocolatey <chocolatey@realdimensions.net>
2023-11-08 08:24:28 +0000

    AU: 2 updated - kmymoney virtualhere-client [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/df83fa4af54af1af3b5f51ef9d1446e016deeef6

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1945-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1946-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '86491189fe1c902aa13cd99405da3ffcff897ac7bffc00d4bd0d68674a6f1314'
+  checksum64     = '5fd513d12ee2856bca82f9ad657a59e2018050492954a1972bbf1007ad89786f'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
