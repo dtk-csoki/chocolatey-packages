ca637af81d45b9b0cc3ff7e6908432164c8034c1
Chocolatey <chocolatey@realdimensions.net>
2023-11-11 08:24:41 +0000

    AU: 4 updated - kmymoney multipar multipar.install multipar.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/81844a0bef106f0c331cd9c3225757c8713c207f

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1948-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1949-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '8607a0a7e5088a2c4b5d7c0741a5525f7d592c4d1043dfcca0981a558fc0f89d'
+  checksum64     = '454aad5ab4fcc909990dfe03ae303f4c4d244cb5d73c14f4908d0dc32fa10dd7'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
