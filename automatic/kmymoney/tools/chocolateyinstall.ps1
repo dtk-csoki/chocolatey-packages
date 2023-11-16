73f1c3a7413453b69da15d6b4bbb848ade4bc8d7
Chocolatey <chocolatey@realdimensions.net>
2023-11-16 08:24:31 +0000

    AU: 1 updated - kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/91b4ff0bea555647d9cac12ccaba55e76178d6b6

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1953-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1954-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '1d94c51f606b7edff7f0aa510e4b9a11890c2d94fa3be5b20e8ed667e41fafbd'
+  checksum64     = '1fdd27fe7912b7a715c4d90c43807fbe6850e9a7ea485786ac95ed2bc22cb625'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
