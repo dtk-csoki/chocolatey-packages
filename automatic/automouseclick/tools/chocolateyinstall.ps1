4ad7bd78d8ac458a6464c6adbdd67e2ee8b6c935
Chocolatey <chocolatey@realdimensions.net>
2023-11-24 16:24:55 +0000

    AU: 3 updated - automouseclick spacedesk-client spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f315551fa1103317c8deaf25a800a0c2fade62f4

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'BD0E5BC89ACFCC89BCCD392288475781608845BF549353129E5EF49A1985DCF3'
+  checksum     = 'EE4BD8299318299DFA504B93E4ACD69D42547BC68BE1C305C589B9F6D82362C4'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
