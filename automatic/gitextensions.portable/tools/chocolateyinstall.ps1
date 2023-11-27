368a856e375bd4d170992c9bd26c6acca79e3201
Chocolatey <chocolatey@realdimensions.net>
2023-11-27 08:25:14 +0000

    AU: 2 updated - gitextensions.portable kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/45e318c5ace66e9c23a93b0b4cf4c6c9bd870439

--- automatic/gitextensions.portable/tools/chocolateyinstall.ps1
+++ automatic/gitextensions.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\GitExtensions-Portable-4.2.0.17591-bf814d4f1.zip"
+  file          = "$toolsDir\GitExtensions-Portable-4.2.1.17611-b0c0b2848.zip"
 }
 
 # Prevent chocolatey from creating shims for supplementary executables
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1964-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1965-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '5de60c44e208f8e993a34cee582b8bc38fc56d5be9ac2abe0d5a0c556d922efe'
+  checksum64     = 'd31d6c13d83a8d8f5c7471402e0476e2df6c5ffc4a2710128fb1f1f7dd0c28cc'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
