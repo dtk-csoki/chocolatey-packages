ded4a5c59852af99c91daaca9bf79bb286547338
Chocolatey <chocolatey@realdimensions.net>
2023-11-28 16:25:01 +0000

    AU: 6 updated - bandlab-assistant chirp chirp.install pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/aedf8fe5b8043a87997991f63d274b2ad221ec2e

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.0.exe'
-  checksum      = '6f84887637ef99e2de75891506c668089e0b71d86c3b6f9d6bd14716b35645171b2b5d23941101e868a7a02a865bddd136d4d15864607b219ba126fc0141cc0f'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.1.exe'
+  checksum      = 'b0c3f51266ae5c759d3038ea8a0d8db0167d89938bef55a9a2137c2003219c55'
   checksumType  = 'sha512'
 
   silentArgs    = '/S'
