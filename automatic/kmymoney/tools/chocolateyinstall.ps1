4b195b9a8dfb15af7bd65998ea13c47449de7771
Chocolatey <chocolatey@realdimensions.net>
2023-11-29 08:24:47 +0000

    AU: 4 updated - kmymoney pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/87cb169722f1e3a6b27b1808375a5e43f1a2a2d5

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1966-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1967-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '506d8cf59afef1b0f4ebb94c1f29cca2bc26eee0ec6a96518ea5c8356d4660e8'
+  checksum64     = '02ea003aeea2b1ee9a54ef9f28bb6e08e7992a13d2586e8b95df170a9b2b4dc5'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
