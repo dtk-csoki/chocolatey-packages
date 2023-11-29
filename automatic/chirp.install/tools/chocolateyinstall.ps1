8a0dcd3333cb9860fb32706184d03042bf6d9b94
Chocolatey <chocolatey@realdimensions.net>
2023-11-29 16:25:14 +0000

    AU: 5 updated - chirp chirp.install slm spacedesk-client spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/4030a03c4d8b1a1813661b1fe213a4edf8a7ec20

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20231128-installer.exe"
+  file          = "$toolsDir\chirp-next-20231129-installer.exe"
   silentArgs    = '/S'
 }
 
