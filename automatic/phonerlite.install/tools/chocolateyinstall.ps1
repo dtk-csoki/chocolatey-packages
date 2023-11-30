2b26f693a603cf71c979afa0fac10dc6402e0687
Chocolatey <chocolatey@realdimensions.net>
2023-11-30 16:26:54 +0000

    AU: 6 updated - phonerlite phonerlite.install phonerlite.portable realpopup spacedesk-client spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/293e96efb197654c6d70109af2deeda5df2400e0

--- automatic/phonerlite.install/tools/chocolateyinstall.ps1
+++ automatic/phonerlite.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName    
-  file          = "$toolsDir\PhonerLiteSetup_3.23.exe"    
+  file          = "$toolsDir\PhonerLiteSetup_3.24.exe"    
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
