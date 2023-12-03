1fc55359be233edccbffe6280183a54d16db819b
Chocolatey <chocolatey@realdimensions.net>
2023-12-03 16:24:51 +0000

    AU: 1 updated - virtualmidisynth [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/dc59c78673cc5272990b9d93da2b078f22af4deb

--- automatic/virtualmidisynth/tools/chocolateyinstall.ps1
+++ automatic/virtualmidisynth/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.13.7.exe"
+  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.13.8.exe"
   silentArgs  = '/S'
 }
 
