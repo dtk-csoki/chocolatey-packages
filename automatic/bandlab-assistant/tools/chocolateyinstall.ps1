bbc7f50c88f6e66f378319b5ba95fb123d3867a0
Chocolatey <chocolatey@realdimensions.net>
2023-12-04 16:25:58 +0000

    AU: 3 updated - bandlab-assistant chirp chirp.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f2945703dd98a6a41676e7b20ba73f2d1d49dd46

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.1.exe'
-  checksum      = 'b0c3f51266ae5c759d3038ea8a0d8db0167d89938bef55a9a2137c2003219c55'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.2.exe'
+  checksum      = '13ea1fd6682db4f9654525fe31c808f1bea6835f3d245dcd5dc7e4fc8cc7ebe4'
   checksumType  = 'sha512'
 
   silentArgs    = '/S'
--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20231129-installer.exe"
+  file          = "$toolsDir\chirp-next-20231204-installer.exe"
   silentArgs    = '/S'
 }
 
