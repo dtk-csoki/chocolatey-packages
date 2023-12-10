4acafc01f49eee778be841ddbeff41aa33f1061e
Chocolatey <chocolatey@realdimensions.net>
2023-12-10 08:24:53 +0000

    AU: 2 updated - geogebra-geometry kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/10867017eb044b11c5b53c1df1ac15ca3ff057ef

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1977-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1978-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '5d9a600ea3b51232e99da331e78a02edefebc49fe37b2e7bc69758ba5a1c0cb2'
+  checksum64     = '7a5c1a33bcf60b7d04087bfdd54f5e2f5118648e1a4baff02a7a065d4785c6c7'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
