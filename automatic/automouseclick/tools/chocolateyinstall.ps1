8f3bd6120aa776c6de5087d9f4451dce9fce386b
Chocolatey <chocolatey@realdimensions.net>
2023-12-12 00:25:28 +0000

    AU: 2 updated - automouseclick helix-alm-client [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/fd4ef0ae84d04c2d664d7e024a423c7eda1774cc

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'F2496E8FA766D0F4B91138A239061B54AAA73EF19A03DC5E3293025D575D5C9E'
+  checksum     = '36521B3D90917488579E4A17BCA557440FA8AF45F68604E33E5115A587A5C5C7'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2023.3.0/ttwinclientinstall.exe'
-  checksum     = '3d998e6b8a98fb7fd97be8f54cc6bf352207620264f004b49c9ac8ea3414ee86'
+  url          = 'https://cdist2.perforce.com/alm/helixalm/r2023.4.0/ttwinclientinstall.exe'
+  checksum     = 'ab1c864c3f1d3799f01c84ca399e9e15449afc0593bc7cfe9469e8b2bd888b58'
   checksumType = 'sha256'
 
   silentArgs   = '-i SILENT LAX_VM resource\jre\bin\java.exe'
