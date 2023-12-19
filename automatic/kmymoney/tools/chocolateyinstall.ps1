78a3de5e684954105717864d218493c782496559
Chocolatey <chocolatey@realdimensions.net>
2023-12-19 08:24:55 +0000

    AU: 3 updated - kmymoney spacedesk-client spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/bdde17bdda2e41e037332f6e7c48d450e9159934

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1986-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://binary-factory.kde.org/job/KMyMoney_Release_win64/lastSuccessfulBuild/artifact/kmymoney-5.1-1987-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '7345820587dca6d3d6662f73d7bdbaee58c07812307a15732318499383d9703b'
+  checksum64     = 'b6cd3bf88f79976f00ee80bc7325fda62b63e76eb4605b1075b01d68f035f377'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
