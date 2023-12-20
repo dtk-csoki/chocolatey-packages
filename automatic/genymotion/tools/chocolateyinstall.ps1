bf811daccb8190b20b396e6f5936a13f3b0f9318
Chocolatey <chocolatey@realdimensions.net>
2023-12-20 16:24:56 +0000

    AU: 3 updated - chirp chirp.install genymotion [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2440267dca072e2d6c5b7d6ed136927b05ee1302

--- automatic/genymotion/tools/chocolateyinstall.ps1
+++ automatic/genymotion/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.genymotion.com/releases/genymotion-3.5.1/genymotion-3.5.1.exe'
-  checksum     = '@{Algorithm=SHA256; Hash=D9F7BFA3E32501C00612EAE55BCCEFEBA5F285E250896F919C2698A826C3F885; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
+  url          = 'https://dl.genymotion.com/releases/genymotion-3.6.0/genymotion-3.6.0.exe'
+  checksum     = '@{Algorithm=SHA256; Hash=FB8F02459CB5EF092B23EC44BA1FCE797A5672E00D57ECF6EDA13CF92317A02F; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
