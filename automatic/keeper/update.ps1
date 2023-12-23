10dfae9bc72ab123ee89c8db16820405337d03af
chtof <chtof62@gmail.com>
2023-12-23 17:50:40 +0100

    Update update.ps1

--- automatic/keeper/update.ps1
+++ automatic/keeper/update.ps1
@@ -1,6 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
+
 function global:au_GetLatest {
      $releases = 'https://keepersecurity.com/desktop_electron/desktop_electron_version.txt'
 	 $version  = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json).version
@@ -23,4 +28,4 @@ function global:au_SearchReplace {
     }
 }
 
-update -noCheckUrl
+update -ChecksumFor None
