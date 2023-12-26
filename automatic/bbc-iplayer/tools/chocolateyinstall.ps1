35261d7d389a5d511f7c81bd2f224ab06a555e62
chtof <chtof62@gmail.com>
2023-12-26 22:36:01 +0100

    Update chocolateyinstall.ps1

--- automatic/bbc-iplayer/tools/chocolateyinstall.ps1
+++ automatic/bbc-iplayer/tools/chocolateyinstall.ps1
@@ -1,29 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$processName = 'BBCiPlayerDownloads'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
+  packageName   = $env:ChocolateyPackageName  
 
   url           = 'https://static.files.bbci.co.uk/iplayer-pc-download-app/releases/win32-x64/BBCiPlayerDownloadsSetup-2.13.18.exe'
   checksum      = '6e97215f314290865f97edec2e50bb2bb54d236c0ce013b1df4c846352ecd649'
   checksumType  = 'sha256'
+  
+  silentArgs    = ''
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-
-  file          = "$toolsDir\bbciplayerdownloads-2.13.18-full.nupkg" 
+# Kill application as it starts automatically
+While (-Not (Get-Process -Name $processName -ErrorAction SilentlyContinue) ) {
+  Sleep 2
 }
-
-Install-ChocolateyZipPackage @packageArgs
-Remove-Item -Path $packageArgs.file
-
-# Install start menu shortcut
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "BBCiPlayerDownloads.lnk"
-$targetPath = Join-Path $toolsDir "lib\net45\BBCiPlayerDownloads.exe"
-Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+Stop-Process -Name $processName
