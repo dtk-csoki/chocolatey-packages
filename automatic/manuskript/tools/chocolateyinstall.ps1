7441ddbee5cf012f31cd8d1830f48ba6b5b397ce
chtof <chtof62@gmail.com>
2024-01-06 11:49:53 +0100

    Update chocolateyinstall.ps1

--- automatic/manuskript/tools/chocolateyinstall.ps1
+++ automatic/manuskript/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
 
   url           = 'https://github.com//olivierkes/manuskript/releases/download/0.16.1/manuskript-0.16.1-windows.zip'
-  checksum      = '7B789067FC92947C089C7BD54324742156F04D7A2D670DE4E5170C87ABF72F3D'
+  checksum      = '7b789067fc92947c089c7bd54324742156f04d7a2d670de4e5170c87abf72f3d'
   checksumType  = 'sha256'
 }
 
