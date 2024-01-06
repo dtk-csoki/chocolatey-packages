bbb0d5668ccd5fa165091b110c0aad172863a40c
chtof <chtof62@gmail.com>
2024-01-06 12:14:04 +0100

    Update chocolateyinstall.ps1

--- automatic/adobe-connect/tools/chocolateyinstall.ps1
+++ automatic/adobe-connect/tools/chocolateyinstall.ps1
@@ -4,8 +4,10 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
 
-  url           = 'https://download.adobe.com/pub/connect/updaters/meeting/10_6/Connect2020_1_5.msi'
-  checksum      = '6834E6ADCB988A43AC2838C7676AF6E2E8823214A3749378EB3103CBE2734EEF'
+  url32         = 'https://www.adobe.com/go/Connect11_32msi'
+  checksum32    = '3587dd228962bb6d376a657185460c7e5bbba43dae91271c889a269cfa178bf4'
+  url64         = 'https://www.adobe.com/go/Connect11msi'
+  checksum64    = 'addf0f579d16e5ca2906eecb966aab6d12a4f2278c982bbf99f2f46df80138c3'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
