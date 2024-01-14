968663560459c677300d95059bf68d5c491c1cac
chtof <chtof62@gmail.com>
2024-01-14 19:37:35 +0100

    Update chocolateyinstall.ps1

--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
-  url64          = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.20.0/TIB_js-studiocomm_6.20.0_windows_x86_64.exe'
-  checksum64     = '9333cfc633de28e95630e085713319fad837e88a775d815cf152dfbe4222b006'
+  url64          = 'https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.20.6/js-studiocomm_6.20.6_windows_x86_64.exe'
+  checksum64     = ''
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
