cd476e4a08154513c56f10e1d1dd7069fb7ef159
chtof <chtof62@gmail.com>
2024-01-14 19:37:46 +0100

    Update update.ps1

--- automatic/tjs/update.ps1
+++ automatic/tjs/update.ps1
@@ -1,16 +1,4 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $releases = 'https://community.jaspersoft.com/project/jaspersoft-studio/releases'
-    $regex    = 'TIB_js-studiocomm_(?<Version>[\d\.]+)_windows_x86_64.exe'
-
-    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
-    $version = $matches.Version
-
-    return @{
-        Version = $version       
-    }
-}
+﻿. $PSScriptRoot\..\tjs.install\update.ps1
 
 function global:au_SearchReplace {
    @{
