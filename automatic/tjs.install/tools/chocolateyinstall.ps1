60f0cc10de07d80a98e7a96d48d2253f5a2535aa
Chocolatey <chocolatey@realdimensions.net>
2024-01-14 19:21:56 +0000

    AU: 2 updated - tjs tjs.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5779f65c06173966ab5393b62929a3938bef09fc

--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.20.6/js-studiocomm_6.20.6_windows_x86_64.exe'
-  checksum64     = ''
+  checksum64     = '3145a78dfd3a19aaeb5cdb85e32643cd925981b4467d9840eef5cd05e0b7375b'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
