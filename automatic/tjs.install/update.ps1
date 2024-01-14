00fa82ff85a45e09e74fc755c7db352b18ce9a9d
chtof <chtof62@gmail.com>
2024-01-14 19:37:41 +0100

    Update update.ps1

--- automatic/tjs.install/update.ps1
+++ automatic/tjs.install/update.ps1
@@ -1,15 +1,15 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases = 'https://community.jaspersoft.com/project/jaspersoft-studio/releases'
-    $regex    = 'TIB_js-studiocomm_(?<Version>[\d\.]+)_windows_x86_64.exe'
+    $releases = 'https://community.jaspersoft.com/files/file/19-jaspersoft%C2%AE-studio-community-edition/'
+    $regex    = 'Jaspersoft® Studio Community Edition (?<Version>[\d\.]+)'
 
-    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regex | Out-Null
     $version = $matches.Version
 
     return @{
         Version = $version
-        URL64   = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-' + $version + '/TIB_js-studiocomm_' + $version + '_windows_x86_64.exe'
+        URL64   = 'https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-' + $version + '/js-studiocomm_' + $version + '_windows_x86_64.exe'
     }
 }
 
@@ -22,4 +22,6 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor 64
+If ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor 64
+}
