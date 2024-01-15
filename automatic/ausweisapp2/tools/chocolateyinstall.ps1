c5992a53199463ecd2efeff177aa39bca8b49947
Chocolatey <chocolatey@realdimensions.net>
2024-01-15 16:40:58 +0000

    AU: 2 updated - ausweisapp2 offset-explorer [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f7f7b9f4bbae2278e198bc05f38b397a03d5c589

--- automatic/ausweisapp2/tools/chocolateyinstall.ps1
+++ automatic/ausweisapp2/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   filetype    = 'MSI'
-  file        = "$toolsDir\AusweisApp-2.0.0.msi"
+  file        = "$toolsDir\AusweisApp-2.0.2.msi"
   silentArgs  = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
--- automatic/offset-explorer/tools/chocolateyinstall.ps1
+++ automatic/offset-explorer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://www.kafkatool.com/download2/offsetexplorer.exe'
-  checksum      = 'fe94ebb1a7746f0b2fb25c8eb51dbb5e8e732e530d156da4c0a4285ca84aced1'
+  checksum      = '736d7e7b53c8051a5fb0594e1cddccc492f3a27a28304229b281b28b35ca335f'
   checksumType  = 'sha256'
 
   url64bit      = 'https://www.kafkatool.com/download2/offsetexplorer_64bit.exe'
-  checksum64    = '783635460d87e1fcd8d90508a9cec9642467a5145341461f2a749cbf9fe6b6b7'
+  checksum64    = '64a22d860a9b397e71c0c1af3f6505d1514dfcd47ca38094af551a76d2c2df79'
   checksumType64= 'sha256'
 
   silentArgs    = '-q'
