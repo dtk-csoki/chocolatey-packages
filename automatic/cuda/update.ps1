91bb3b9d8bced930c9c10e5eddd64f45b3cf38e4
chtof <chtof62@gmail.com>
2024-01-20 11:10:41 +0100

    Update update.ps1

--- automatic/cuda/update.ps1
+++ automatic/cuda/update.ps1
@@ -1,17 +1,16 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases = 'https://developer.nvidia.com/cuda-downloads'
-    # $regex    = '(/(?<MajorVersion>[\d\.]+)\\?(/Prod)?\\?/local_installers\\?/(?<File>cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d\.]+)_win10.exe))'
-    # cuda_11.6.0_511.23_windows.exe
-    #https:\/\/developer.download.nvidia.com\/compute\/cuda\/11.6.0\/local_installers\/cuda_11.6.0_511.23_windows.exe\u0022
-    $regex    = '(/(?<MajorVersion>[\d\.]+)\\?(/Prod)?\\?/local_installers\\?/(?<File>cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d\.]+)_windows.exe))'
-    
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match $regex | Out-Null
+    $releases  = 'https://developer.nvidia.com/cuda-downloads.json'
+    $regex_url = "https://developer.download.nvidia.com/compute/cuda/(?<MajorVersion>[\d\.]+)/local_installers/cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d]+)(.\d+)?_windows.exe"
+    $json = (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json 
+    $json.data.releases.'Windows/x86_64/11/exe_local'.filename
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regex_url | Out-Null
 
     return @{
         Version = $matches.VersionMajor + '.' + ($matches.VersionMinor -replace '\.','')
-        URL32   = 'https://developer.download.nvidia.com/compute/cuda' + $matches.0 -Replace '\\',''
+        URL32   = $matches.0
     }
 }
 
