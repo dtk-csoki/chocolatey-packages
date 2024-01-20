e15e1cb15d83743a88b7e246394a828aa4c0a9fe
chtof <chtof62@gmail.com>
2024-01-20 22:15:55 +0100

    Update update.ps1

--- automatic/botpress/update.ps1
+++ automatic/botpress/update.ps1
@@ -6,14 +6,18 @@ function global:au_GetLatest {
     $regex   = '/botpress/botpress/releases/tag/v(?<Version>[\d\.]*)"'
 	(Invoke-WebRequest -Uri $releases -UseBasicParsing) -match $regex | out-null
 	$version_download = $matches.Version -replace "\.", "_"
-	return @{ Version = $matches.Version ; URL = $releases_download + "botpress-v" + $version_download + "-win-x64.zip" ; zipFileName = "botpress-v" + $version_download + "-win-x64.zip" }
+	return @{
+        Version = $matches.Version
+        URL64 = $releases_download + "botpress-v" + $version_download + "-win-x64.zip"
+        zipFileName = "botpress-v" + $version_download + "-win-x64.zip"
+    }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }    
         "tools\chocolateyuninstall.ps1" = @{
             '(.*zipFileName\s*=\s*)(".*")' = "`$1`"$($Latest.zipFileName)`""
