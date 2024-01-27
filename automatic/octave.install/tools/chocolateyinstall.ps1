38e255e620742df132c4983920d1d89cfdc07b1d
chtof <chtof62@gmail.com>
2024-01-27 09:34:42 +0100

    Update chocolateyinstall.ps1

--- automatic/octave.install/tools/chocolateyinstall.ps1
+++ automatic/octave.install/tools/chocolateyinstall.ps1
@@ -5,16 +5,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  url           = 'https://ftpmirror.gnu.org/octave/windows/octave-8.3.0-w32-installer.exe'
-  checksum      = '2d6062a5a33f49ecd33cc21811cd030e8a93b8afb67a217d985a03d88275bb92'
-  checksumType  = 'sha256'
-
   url64         = 'https://ftpmirror.gnu.org/octave/windows/octave-8.3.0-w64-installer.exe'
   checksum64    = '214e33138aba18f9a97d1fc7e326707a6ecacc7a59d70fbe13445b96506bdfab'
   checksumType64= 'sha256'
 
   # Note: silentArgs doesn't work as expected for octave.install
-  # An autohotkey is used as workaround.
+  # An autohotkey script is used as workaround.
   # silentArgs	= "/S"
 }
 
