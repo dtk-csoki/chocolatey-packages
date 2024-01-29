3b3953f199f1ccbe3a9686961e56e3e9f67d4e4a
Chocolatey <chocolatey@realdimensions.net>
2024-01-29 16:40:57 +0000

    AU: 2 updated - bandlab-assistant roomarranger [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/99347e7ed8ca39a2ea34535685d8a7c301942f41

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.2.exe'
-  checksum      = '13ea1fd6682db4f9654525fe31c808f1bea6835f3d245dcd5dc7e4fc8cc7ebe4'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.3.exe'
+  checksum      = '4d6470c43efb70d23091f0b4ee22213fadf1a39075b24b6b609ca1e9c4cc22a9'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
--- automatic/roomarranger/tools/chocolateyinstall.ps1
+++ automatic/roomarranger/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"  
-  file64      = "$toolsDir\rooarr981_64bit.exe"
+  file64      = "$toolsDir\rooarr982_64bit.exe"
   silentArgs	= '/S'
 }
 
