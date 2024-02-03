f9c61241aa000472c30943c88fc535fd51d12620
chtof <chtof62@gmail.com>
2024-02-03 10:19:23 +0100

    [deprecated] Gravit Designer Pro is now Corel Vector

--- automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
@@ -1,13 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-
-  url           = 'https://designer.gravit.io/_downloads/windows/GravitDesignerInstaller.exe'
-  checksum      = '8fc0f0acc64b1d22c504b7390ec5c3d27b5c980165c14be429d7497ccedefb6c'
-  checksumType  = 'sha256'
-
-  silentArgs    = '/S'
-}
-
-Install-ChocolateyPackage @packageArgs
+﻿
