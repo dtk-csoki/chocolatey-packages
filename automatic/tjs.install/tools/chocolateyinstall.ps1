db1d99784be66ec958cfe90479bf9ec553c62477
chtof <chtof62@gmail.com>
2024-02-03 23:05:37 +0100

    Update chocolateyinstall.ps1

--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
-  url64          = 'https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.20.6/js-studiocomm_6.20.6_windows_x86_64.exe'
-  checksum64     = '3145A78DFD3A19AAEB5CDB85E32643CD925981B4467D9840EEF5CD05E0B7375B'
+  url64          = Get-Redirectedurl 'http://sourceforge.net/projects/jasperstudio/files/JaspersoftStudio-6.21.0/js-studiocomm_6.21.0_windows_x86_64.exe/download'
+  checksum64     = 'f596f3334b091206b1c8262ad98a827fba46a56e8a8306c753e5fdcb872f17dc'
   checksumType64 = 'sha256'
 
-  silentArgs     = "/S"
+  silentArgs     = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
