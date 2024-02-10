62fc897cea939add7b0d45f5eb6b0bdd2084bd6f
chtof <chtof62@gmail.com>
2024-02-10 11:53:26 +0100

    [deprecated] tjs
    Starting January 25, 2024, JaspersoftⓇ will no longer offer JasperReportsⓇ Server community edition as part of its free downloads. If you would like to try JasperReports Server to embed or distribute reports and dashboards, as well as access other advanced features, you can take a 30-day trial to the Jaspersoft commercial edition.
    
    The new free Jaspersoft community edition will include JasperReports Library and Jaspersoft Studio, with a one-time required login after downloading Jaspersoft Studio.
    
    Please visit their FAQs for more information, or if you have any questions, you can send a message through their Contact Us link at the bottom of their community website page https://community.jaspersoft.com/downloads/community-edition

--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -1,13 +1,1 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-
-  url64          = Get-Redirectedurl 'http://sourceforge.net/projects/jasperstudio/files/JaspersoftStudio-6.21.0/js-studiocomm_6.21.0_windows_x86_64.exe/download'
-  checksum64     = 'f596f3334b091206b1c8262ad98a827fba46a56e8a8306c753e5fdcb872f17dc'
-  checksumType64 = 'sha256'
-
-  silentArgs     = '/S'
-}
-
-Install-ChocolateyPackage @packageArgs
+﻿
--- automatic/tjs.portable/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-# Remove start menu shortcut
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "JasperSoft Studio.lnk"
-if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
