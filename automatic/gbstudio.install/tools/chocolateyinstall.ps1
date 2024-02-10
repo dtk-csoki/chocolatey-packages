2477a7afd57b999ad82fbbb164684b540e072297
Chocolatey <chocolatey@realdimensions.net>
2024-02-10 09:50:39 +0000

    AU: 2 updated - gbstudio.install gbstudio.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b1354604e35f8d54f265abad1fb636c64c7a680e

--- automatic/gbstudio.install/tools/chocolateyinstall.ps1
+++ automatic/gbstudio.install/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
 
-  url           = 'https://github.com//chrismaltby/gb-studio/releases/download/v1.2.1/GB-Studio-Windows-32bit-squirrel-1.2.1.zip'
-  checksum      = '5c3852eabe4fd8f7ad2dd263f607641ecd031a8c3f40c0ab212677e4acfc2846'
+  url           = 'https://github.com//chrismaltby/gb-studio/releases/download/v3.2.0/gb-studio-windows-32bit-installer.zip'
+  checksum      = 'ed04d4ee0de4626693aedfdfd2eebaeff5b2ff1b71d408f138026b6095c366f7'
   checksumType  = 'sha256'
   
-  url64         = 'https://github.com//chrismaltby/gb-studio/releases/download/v1.2.1/GB-Studio-Windows-64bit-squirrel-1.2.1.zip'
-  checksum64    = '815362639328196850b71728c9c48aa9507aa19024476b4292c66ae8a60eb1e8'
+  url64         = 'https://github.com//chrismaltby/gb-studio/releases/download/v3.2.0/gb-studio-windows-64bit-installer.zip'
+  checksum64    = '6602b647a2ea1a4cfff9ac1c7addbf6a1120bd88e1301a077381cb4aadd7a612'
   checksumType64= 'sha256'
 }
 
@@ -18,8 +18,8 @@ Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file32      = "$toolsDir\gb-studio-master-windows_x86-squirrel\GB Studio-1.2.1 Setup.exe"
-  file64      = "$toolsDir\gb-studio-master-windows_x86_64-squirrel\GB Studio-1.2.1 Setup.exe"
+  file32      = "$toolsDir\gb-studio-master-windows_x86-squirrel\GB Studio-3.2.0 Setup.exe"
+  file64      = "$toolsDir\gb-studio-master-windows_x86_64-squirrel\GB Studio-3.2.0 Setup.exe"
   silentArgs  = '--silent'
 }
 Install-ChocolateyInstallPackage @packageArgs
