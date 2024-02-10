672393bca9d480f8bfaad586a9892fd42fb2e46a
chtof <chtof62@gmail.com>
2024-02-10 10:29:28 +0100

    Create update.ps1

---
+++ automatic/gbstudio.portable/update.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+. ..\..\helpers\GitHub_Helper.ps1
+
+function global:au_BeforeUpdate {    
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+}
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'chrismaltby/gb-studio'
+        regex32  = '/gb-studio-windows-32bit-standalone.zip$'
+        regex64    = '/v(?<Version>[\d\.]+)/gb-studio-windows-64bit-standalone.zip$'
+   }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -ChecksumFor none
