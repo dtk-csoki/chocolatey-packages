f5dfe8acba811f4f44f231cf90c59e128a05321e
chtof <chtof62@gmail.com>
2024-02-17 19:41:32 +0100

    Update update.ps1

--- automatic/chirp.portable/update.ps1
+++ automatic/chirp.portable/update.ps1
@@ -5,7 +5,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {	
     $releases = "https://trac.chirp.danplanet.com/download?stream=next"
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regex   = '^chirp-next-(?<date_url>\d+)-win32.zip$'
+    $regex   = '^chirp-next-(?<date_url>\d+)-win64.zip$'
     $download_page.links | ? href -match $regex
     $version_YY = $matches.date_url.substring(0, $matches.date_url.length-4)
     $version_MM = $matches.date_url.substring($matches.date_url.length-4, 2)
@@ -13,19 +13,19 @@ function global:au_GetLatest {
     $version = $version_YY + "." + $version_MM + "." + $version_DD
     return @{
         Version = $version
-        URL32 = 'https://trac.chirp.danplanet.com/chirp_next/next-' + $matches.date_url + "/chirp-next-" + $matches.date_url + '-win32.zip' }
+        URL64   = 'https://trac.chirp.danplanet.com/chirp_next/next-' + $matches.date_url + "/chirp-next-" + $matches.date_url + '-win64.zip' }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"            
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"            
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
