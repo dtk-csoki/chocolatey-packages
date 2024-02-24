4a78b024ce16c3dd05a0476848526118c7aee01c
Chocolatey <chocolatey@realdimensions.net>
2024-02-24 16:25:10 +0000

    AU: 4 updated - automouseclick chirp chirp.install chirp.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2175470462a7d1d100a8f94ff0b46b3a3903f4ac

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'DE71A0D68FE27D09D0E858629D7D3CCADC884041F28A3D39C02DAF71FE32B118'
+  checksum     = '1D594BCEDE9A8A912BE1D49767486F9758FE67056BE1FCEB0065F7B895FB56F0'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
