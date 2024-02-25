3b61515bc6199e77a807dbeef33c818899662051
Chocolatey <chocolatey@realdimensions.net>
2024-02-25 00:24:45 +0000

    AU: 3 updated - extraterm extraterm.install extraterm.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5c492557908bd2662fdf5de9920153d298cf815d

--- automatic/extraterm.install/tools/chocolateyinstall.ps1
+++ automatic/extraterm.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\extratermqt-setup-0.75.0.exe"
+  file        = "$toolsDir\extratermqt-setup-0.76.0.exe"
   silentArgs  = "/S"
 }
 
