5e8aa66088d5ecffa5ad20574ae3f3b2e29a84b9
Chocolatey <chocolatey@realdimensions.net>
2024-03-06 00:29:10 +0000

    AU: 2 updated - cuda webex-meetings [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e4e4087582fed96e03572c3d7cdf72d3132a10d5

--- automatic/cuda/tools/chocolateyinstall.ps1
+++ automatic/cuda/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda_12.3.2_546.12_windows.exe'
-  checksum     = '3a4b476e1cd70eaabe42881b480f48557481d1ede0ab5f33b2af11b8903425b8'
+  url          = 'https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda_12.4.0_551.61_windows.exe'
+  checksum     = 'd72ca1cdfd403966cd1f67f57c21a768cadbbd2cd48efff5edaa2164c10c4cc8'
   checksumType = 'sha256'
 
   silentArgs   = '/s'
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = 'e9819a5969d1395a63c4f3d59098dfdb449b4330af510d2bea7fb377466144a0be3438c23d62de95f2b299ec41b907fb9a83c4874db602967a99900e63ba6cea'
+  checksum     = '141f0489e01d764118a44273da2bbfed25de5883f624fd68c8c4ccc4c1e9d244e8eddf22811aeecbfa4cc2b9f3431703c97a799065be31d6a37da34df1c568fe'
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
