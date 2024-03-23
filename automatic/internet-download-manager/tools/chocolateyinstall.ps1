6f1f73bf02586f50f6687af138426b343ec74e90
Chocolatey <chocolatey@realdimensions.net>
2024-03-23 16:30:30 +0000

    AU: 1 updated - internet-download-manager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/785907b45f4f9dcb0994726fddc015975d348c79

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build5.exe'
-  checksum      = '780c7ffaf8640824510ab16f0e2c037982491a8e6ca6e0a66d046ea0024b6dd9'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build6.exe'
+  checksum      = '2de950a9212761131c92eba3fd5452439cbe84b62fc48fa3663ac14be0622be1'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
