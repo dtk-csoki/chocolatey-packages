53053f188b7316bd62b33fc77e98f909a461904b
Chocolatey <chocolatey@realdimensions.net>
2024-04-02 16:28:54 +0000

    AU: 4 updated - automouseclick geogebra6 geogebra-geometry texmacs [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ddc8d9d3756015c2949d0d818eee2658b6abec39

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '35CC890113D90C1964B4B054FADD15771E6E592EDC9C5178236E88B7F3B3CF25'
+  checksum     = 'E535703F4613E459CDBBBDF263E0518020D84EEA88F98E063E04A1864D5023AD'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
