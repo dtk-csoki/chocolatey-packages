ddfed5a65368b5efdc517a738a9e67aa279ddfa3
Chocolatey <chocolatey@realdimensions.net>
2024-04-16 16:41:15 +0000

    AU: 2 updated - ausweisapp2 moneydance [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/7ca7f2b519d04d45563fb48ead95999533886d45

--- automatic/moneydance/tools/chocolateyinstall.ps1
+++ automatic/moneydance/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
-  checksum64     = 'e5babb96a5d015c302799023769c7d4edd2be0bca40ac44e7079ffeec5ff1a1e'
+  checksum64     = '425f12561b8235e3f4192bcf43b3f903f24c8c2eda3612836aa3aeea1e335014'
   checksumType64 = 'sha256'
 
   silentArgs     = "-q"
