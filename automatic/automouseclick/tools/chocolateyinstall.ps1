b659645c04daf169b9ffb2146c205000043f2ae4
Chocolatey <chocolatey@realdimensions.net>
2024-04-19 00:41:00 +0000

    AU: 2 updated - automouseclick gnuplot [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/258301d4ca55582ceec29710a3eb0a9b043198f6

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'B1EDAC297D968B607B801FC6F0EDA6A3435AF0847AA5A781B415203983849EC6'
+  checksum     = 'BD5EA10E90D5CB455ACD9590136E3B0FBD16E6A6FFA7F7A7217EEBC96BD9263F'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
