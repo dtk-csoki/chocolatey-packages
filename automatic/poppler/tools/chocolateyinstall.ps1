28260a28e687119b82da7b1e818c8fa9ec13b4aa
chtof <chtof62@gmail.com>
2024-04-21 17:45:15 +0200

    Update chocolateyinstall.ps1

--- automatic/poppler/tools/chocolateyinstall.ps1
+++ automatic/poppler/tools/chocolateyinstall.ps1
@@ -6,5 +6,14 @@ $packageArgs = @{
   destination = "$toolsDir"
   file64      = "$toolsDir\poppler-22.11.0-ha6c1112_0.tar.bz2"  
 }
-
 Get-ChocolateyUnzip @packageArgs
+
+$packageArgs_tar = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file64      = "$toolsDir\$(Get-Item ($packageArgs.file64)).BaseName"   # tar file
+}
+Get-ChocolateyUnzip @packageArgs_tar
+
+Remove-Item $packageArgs.file64
+Remove-Item $packageArgs_tar.file64
