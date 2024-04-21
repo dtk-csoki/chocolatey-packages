c66b2991bbb7fe325c70ed3fb12672dc787be8d8
chtof <chtof62@gmail.com>
2024-04-21 17:45:01 +0200

    Update chocolateyinstall.ps1

--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"
-  file64        = "$toolsDir\chirp-next-20240322-win64.zip"
+  file64        = "$toolsDir\chirp-next-20240419-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
