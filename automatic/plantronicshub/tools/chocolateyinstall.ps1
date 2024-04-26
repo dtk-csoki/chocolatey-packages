92c36d232b428b95ce10f4749b2ac0f6f2c479df
dtk-csoki <141942401+dtk-csoki@users.noreply.github.com>
2024-04-26 09:43:35 -0400

    Updated download url. The prior url was defunct

--- automatic/plantronicshub/tools/chocolateyinstall.ps1
+++ automatic/plantronicshub/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
+  url          = 'https://downloads.poly.com/headsets/PlantronicsHubInstaller.exe'
   checksum     = 'e43484d770f3da729d8ba090604ad971a3db201ec7dfa80ad21cfe3cd3b38a52'
   checksumType = 'sha256'
 
